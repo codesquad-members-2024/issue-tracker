fa1968ca6b4295176360241668aa7462c8249cd9
jueun-park98 <jueun-park@github.com>
2024-05-08 11:39:32 +0900

    feat: 로그인 입력창 애니메이션 추가

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import pageLogo from "../../img/pageLogo.svg";
+import LoginForm from "./LoginForm";
 
 function Login() {
   return (
@@ -7,12 +8,8 @@ function Login() {
       <img src={pageLogo} />
       <GithubLoginButton>Github 계정으로 로그인</GithubLoginButton>
       <div>or</div>
-      <IdForm>
-        <IdLabel>아이디</IdLabel>
-      </IdForm>
-      <PasswordForm>
-        <PasswordLabel>비밀번호</PasswordLabel>
-      </PasswordForm>
+      <LoginForm type="id" />
+      <LoginForm type="password" />
       <LoginButton>아이디로 로그인</LoginButton>
       <RegistrationButton>회원가입</RegistrationButton>
     </LoginWrapper>
@@ -91,6 +88,67 @@ const RegistrationButton = styled.button`
   background-color: transparent;
   font-size: 16px;
   color: #4E4B66;
+`;
+
+const StyledParagraph = styled.p`
+  position: relative;
+  width: 100%;
+  height: 24px;
+  margin: 0;
+`;
+
+const StyledInput = styled.input`
+  position: relative;
+  top: 35%;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: 0 none;
+  background-color: transparent;
+  color: #595f63;
+  outline: none;
+
+  &:focus + label span,
+  &:valid + label span {
+    transform: translateY(-60%);
+    font-size: 12px;
+    color: #595f63;
+  }
+
+  &:focus + label::after,
+  &:valid + label::after {
+    width: 100%;
+    transform: translateX(0);
+  }
+`;
+
+const StyledLabel = styled.label`
+  position: absolute;
+  top: -50%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  text-align: left;
+  pointer-events: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -1px;
+    width: 0;
+    height: 100%;
+    border: none;
+    transition: all .3s ease;
+  }
+
+  span {
+    position: absolute;
+    top: 1em;
+    left: 0;
+    bottom: 5px;
+    transition: all .3s ease;
+  }
 `;
 
 export default Login;
---
+++ frontend/src/components/login/LoginForm.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import styled, { css } from "styled-components";
+
+interface LoginFormProps {
+  type: "id" | "password";
+}
+
+const LABEL_TEXT = {
+  id: "아이디",
+  password: "비밀번호",
+};
+
+function LoginForm({ type }: LoginFormProps) {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  return (
+    <Form isFocused={isFocused}>
+      <StyledParagraph>
+        <StyledInput type="text" required onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} />
+        <StyledLabel>
+          <span>{LABEL_TEXT[type]}</span>
+        </StyledLabel>
+      </StyledParagraph>
+    </Form>
+  );
+}
+
+const Form = styled.div<{ isFocused: boolean }>`
+  width: 288px;
+  height: 56px;
+  display: flex;
+  padding: 0 1em;
+  flex-direction: column;
+  justify-content: center;
+  border: 1px solid #eceef5;
+  border-radius: 16px;
+  background-color: #eceef5;
+  transition: all 0.5s ease;
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      background-color: white;
+      outline: 1.6px solid black;
+    `}
+`;
+
+const StyledParagraph = styled.p`
+  position: relative;
+  width: 100%;
+  height: 24px;
+  margin: 0;
+`;
+
+const StyledInput = styled.input`
+  position: relative;
+  top: 35%;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: 0 none;
+  background-color: transparent;
+  color: #595f63;
+  outline: none;
+
+  &:focus + label span,
+  &:valid + label span {
+    transform: translateY(-60%);
+    font-size: 12px;
+    color: #595f63;
+  }
+
+  &:focus + label::after,
+  &:valid + label::after {
+    width: 100%;
+    transform: translateX(0);
+  }
+`;
+
+const StyledLabel = styled.label`
+  position: absolute;
+  top: -50%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  text-align: left;
+  pointer-events: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -1px;
+    width: 0;
+    height: 100%;
+    border: none;
+    transition: all 0.3s ease;
+  }
+
+  span {
+    position: absolute;
+    top: 1em;
+    left: 0;
+    bottom: 5px;
+    transition: all 0.3s ease;
+  }
+`;
+
+export default LoginForm;
