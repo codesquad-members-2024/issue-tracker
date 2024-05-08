e56d65c1ef4fdf4bcda3d11b95a8075344bfe6ad
kalia <fmoon11200@naver.com>
2024-05-08 17:04:34 +0900

    feature 회원가입 시 입력한 정보 길이 검증 verifyUserinfo 추가

--- src/main/java/com/CodeSquad/IssueTracker/user/UserService.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/UserService.java
@@ -1,8 +1,10 @@
 package com.CodeSquad.IssueTracker.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -12,7 +14,36 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void save(User user){
-        userRepository.save(user);
+    public String save(User user){
+        log.info("사용자 저장 시도: {}", user.getUserId());
+        if (verifyUserInfo(user)) {
+            userRepository.save(user);
+            log.info("사용자 저장 성공: {}", user.getUserId());
+            return "success";
+        }
+        else{
+            log.error("사용자 정보 검증 실패: {}", user.getUserId());
+            return "fail";
+        }
+    }
+
+    private boolean verifyUserInfo(User user){
+        if(!isLengthVaild(user.getUserId(), 6, 16)){
+            log.error("사용자 ID 길이가 유효하지 않음: {}", user.getUserId());
+            return false;
+        }
+        if(!isLengthVaild(user.getUserPassword(), 6 , 12)){
+            log.error("사용자 비밀번호 길이가 유효하지 않음: {}", user.getUserId());
+            return false;
+        }
+        if(!isLengthVaild(user.getUserNickname(), 2, 6)){
+            log.error("사용자 닉네임 길이가 유효하지 않음: {}", user.getUserId());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isLengthVaild(String field, int min, int max){
+        return field.length() >= min && field.length() <= max;
     }
 }
