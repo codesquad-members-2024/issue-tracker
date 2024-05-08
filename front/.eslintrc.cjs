52f89eafb3968d0e3e41e25793e349808ac8dd53
melroh629 <jw.roh629@gmail.com>
2024-05-08 12:53:27 +0900

    chore: react-router 추가, 불필요한 파일 삭제

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,21 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react/jsx-no-target-blank': 'off',
+		'react/prop-types': 'off',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'no-unused-vars': 'warn',
+		'arrow-parens': ['warn', 'as-needed'],
+		'arrow-body-style': ['warn', 'as-needed'],
+		'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+	},
+};
