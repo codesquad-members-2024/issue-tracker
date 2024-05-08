0b9933a90432f1e3546bd370c778ae11b2a0682c
곽태영 <amberaplin14@gmail.com>
2024-05-08 18:09:39 +0900

    [feat/#1] feat: 사용자 등록 구현
    - 사용자 등록 시 생성시간, 수정시간 자동 등록 테스트

---
+++ src/main/java/com/issuetracker/domain/member/Member.java
@@ -0,0 +1,29 @@
+package com.issuetracker.domain.member;
+
+import com.issuetracker.domain.common.BaseDateTime;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseDateTime implements Persistable<String> {
+
+    @Id
+    @Column("MEMBER_ID")
+    private String id;
+    private String password;
+
+    @Transient
+    @Builder.Default
+    private boolean isNew = true;
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
+}
---
+++ src/main/java/com/issuetracker/domain/member/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.issuetracker.domain.member;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends CrudRepository<Member, String> {
+}
