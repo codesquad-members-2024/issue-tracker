aed8742b3a71f870a5a11dcd7f32eaf13e2296cb
Daniel <923daniel@naver.com>
2024-05-08 13:21:48 +0900

    chore: 로컬 셋팅
    - gitignore 설정.
    - build.gradle 설정.

---
+++ back/src/main/java/codesquad/issuetracker/Team04Application.java
@@ -0,0 +1,13 @@
+package codesquad.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team04Application.class, args);
+	}
+
+}
