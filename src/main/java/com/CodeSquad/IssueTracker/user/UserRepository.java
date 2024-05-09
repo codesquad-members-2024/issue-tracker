60c46c09e4331c9504bcdaea5baacda224a6736e
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-09 10:15:12 +0900

    feat 유저 ID를 통해 DB에서 원하는 유저를 맵핑해서 가져오는 기능 추가

--- src/main/java/com/CodeSquad/IssueTracker/user/UserRepository.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/UserRepository.java
@@ -1,8 +1,11 @@
 package com.CodeSquad.IssueTracker.user;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserRepository {
@@ -13,7 +16,21 @@ public class UserRepository {
     }
 
     public void save(User user) {
-        String sql = "insert into IssueTracker.USERS (USER_ID, USER_PASSWORD, USER_NICKNAME) values (?, ?, ?)";
+        String sql = "insert into IssueTracker.users (userId, userPassword, userNickname) values (?, ?, ?)";
         jdbcTemplate.update(sql,user.getUserId(),user.getUserPassword(),user.getUserNickname());
+    }
+
+    public Optional<User> findUserById(String userId) {
+        String sql = "SELECT * FROM IssueTracker.users WHERE userId = ?";
+        List<User> user = jdbcTemplate.query(sql, userRowMapper(), userId);
+        return user.stream().findAny();
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return (rs, rowNum) -> new User(
+                rs.getString("userId"),
+                rs.getString("userPassword"),
+                rs.getString("userNickname")
+        );
     }
 }
