5e4a0a15a5edecb00d33be0ea7eb57d97b93eac5
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-09 10:10:11 +0900

    feat user의 기본 생성자와 초기화된 생성자 추가

--- src/main/java/com/CodeSquad/IssueTracker/user/User.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/User.java
@@ -7,4 +7,13 @@ public class User {
     private String userId;
     private String userPassword;
     private String userNickname;
+
+    public User(){
+    }
+
+    public User(String userId, String userPassword, String userNickname) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userNickname = userNickname;
+    }
 }
