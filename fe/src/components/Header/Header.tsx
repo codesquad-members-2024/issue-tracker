92c0c8082cc5f3d3e69a3e8833dbd109ad34f190
mi-hye <satge13@gmail.com>
2024-05-09 16:59:52 +0900

    feat: 유저 프로필 메뉴가 아닌 곳을 누르면 메뉴바 숨김 기능 구현

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -10,6 +10,7 @@ interface Props {
 
 function Header({ darkMode, setDarkMode }: Props) {
 	const [isVisible, setIsVisible] = useState(false);
+
 	const LogoComponent = useMemo(() => {
 		const Logo = darkMode ? LogoWhite : LogoBlack;
 		return <Logo className="cursor-pointer" onClick={() => window.location.reload()} />;
@@ -19,7 +20,7 @@ function Header({ darkMode, setDarkMode }: Props) {
 		<div className="my-2 flex justify-between items-center">
 			{LogoComponent}
 			<div className="relative flex justify-between items-center">
-				<button onClick={() => setIsVisible((prev) => !prev)}>
+				<button className="z-10" onClick={() => setIsVisible((prev) => !prev)}>
 					{/*TODO : db의 유저프로필로 변경예정 */}
 					<img
 						className="w-[32px] h-[32px] rounded-full"
@@ -27,7 +28,7 @@ function Header({ darkMode, setDarkMode }: Props) {
 						src="https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__"
 					></img>
 				</button>
-				{isVisible && <ProfileMenu darkMode={darkMode} setDarkMode={setDarkMode} />}
+				{isVisible && <ProfileMenu darkMode={darkMode} setDarkMode={setDarkMode} setIsVisible={setIsVisible}/>}
 			</div>
 		</div>
 	);
--- fe/src/components/Header/ProfileMenu/ProfileMenu.tsx
+++ fe/src/components/Header/ProfileMenu/ProfileMenu.tsx
@@ -4,23 +4,31 @@ import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 interface Props {
 	darkMode: string;
 	setDarkMode: React.Dispatch<React.SetStateAction<string>>;
+	setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function ProfileMenu({ darkMode, setDarkMode }: Props) {
+function ProfileMenu({ darkMode, setDarkMode, setIsVisible }: Props) {
 	return (
-		<div className="absolute top-10 right-0 w-[170px] h-[100px] rounded-xl shadow-profile dark:bg-gray-800">
-			<div className="w-full h-1/2 border-b-[1px] text-grayscale.700 flex items-center justify-between dark:text-grayscale.400 dark:border-grayscale.600">
-				<span className="mx-3 text-sm">모드전환</span>
-				<FontAwesomeIcon
-					icon={darkMode ? faSun : faMoon}
-					onClick={() => setDarkMode((prev) => (prev ? "" : "dark"))}
-					className="mr-5 text-xl cursor-pointer hover:animate-wiggle dark:text-yellow-400"
-				/>
+		<>
+			<div
+				className="fixed top-0 left-0 w-screen h-screen z-0"
+				onClick={() => setIsVisible(false)}
+			></div>
+
+			<div className="absolute top-10 right-0 w-[170px] h-[100px] rounded-xl shadow-profile dark:bg-gray-800 z-10">
+				<div className="w-full h-1/2 border-b-[1px] text-grayscale.700 flex items-center justify-between dark:text-grayscale.400 dark:border-grayscale.600">
+					<span className="mx-3 text-sm cursor-default">모드전환</span>
+					<FontAwesomeIcon
+						icon={darkMode ? faSun : faMoon}
+						onClick={() => setDarkMode((prev) => (prev ? "" : "dark"))}
+						className="mr-5 text-xl cursor-pointer hover:animate-wiggle dark:text-yellow-400"
+					/>
+				</div>
+				<div className="w-full h-1/2 text-grayscale.700 flex items-center cursor-pointer hover:font-black dark:text-grayscale.400">
+					<button className="mx-3 text-sm transition-[font-weight]">로그아웃</button>
+				</div>
 			</div>
-			<div className="w-full h-1/2 text-grayscale.700 flex items-center dark:text-grayscale.400">
-				<button className="mx-3 text-sm hover:font-black transition-[font-weight]">로그아웃</button>
-			</div>
-		</div>
+		</>
 	);
 }
 export default ProfileMenu;
