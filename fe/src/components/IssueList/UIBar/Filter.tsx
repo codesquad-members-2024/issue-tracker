0d0978eb336aaa85af27796319630ac66998eee8
mi-hye <satge13@gmail.com>
2024-05-09 20:43:16 +0900

    design: border 수정

--- fe/src/components/IssueList/UIBar/Filter.tsx
+++ fe/src/components/IssueList/UIBar/Filter.tsx
@@ -7,7 +7,7 @@ const textColor = "text-grayscale.700 dark:text-grayscale.400";
 function Filter() {
 	return (
 		<div className="component-border h-[40px] w-2/5 flex items-center dark:component-border--dark">
-			<details className="w-[20%] min-w-[90px] relative h-full cursor-pointer rounded-l-xl hover:bg-grayscale.200 dark:hover:bg-grayscale.700">
+			<details className="w-[20%] min-w-[90px] relative h-full cursor-pointer rounded-l-xl border-r-2 border-grayscale.300 hover:bg-grayscale.200 dark:border-grayscale.600 dark:hover:bg-grayscale.700">
 				<summary className="h-full flex justify-between items-center">
 					<span className={`ml-5 ${textColor}`}>필터</span>
 					<FontAwesomeIcon icon={faAngleDown} className={`mr-5 ${textColor}`} />
@@ -16,7 +16,7 @@ function Filter() {
 			</details>
 			<div className="relative w-[80%] h-full ">
 				<input
-					className={`absolute top-0 h-full w-full ${textColor} rounded-r-xl pl-9 bg-grayscale.200 border-l-2 border-grayscale.300 dark:bg-grayscale.700 dark:component-border--dark focus:input-text--focus`}
+					className={`absolute top-0 h-full w-full ${textColor} rounded-r-xl pl-9 bg-grayscale.200 dark:bg-grayscale.700 focus:input-text--focus`}
 					type="text"
 				/>
 				<FontAwesomeIcon icon={faMagnifyingGlass} className={`absolute top-3 mx-3 ${textColor}`} />
