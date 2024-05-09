3026295596aabc3bce0e7e40f36914d82369b730
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-09 14:25:48 +0900

    feat 로그인 될때 session을 생서 및 응답 추가

--- src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
@@ -1,5 +1,7 @@
 package com.CodeSquad.IssueTracker.user;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,15 +30,24 @@ public class UserController {
     }
 
     @PostMapping("/post/login")
-    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest,
+                                       HttpServletRequest request){
+        if (loginRequest == null){
+            log.warn("로그인 실패: 로그인 데이터가 없습니다.");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         String userId = loginRequest.getUserId();
         String userPassword = loginRequest.getUserPassword();
 
         log.info("로그인 시도: {}", userId);
 
         if (userService.authenticate(userId, userPassword)){
+            HttpSession session = request.getSession(true);
+            session.setMaxInactiveInterval(1800);
+            session.setAttribute("userId", userId);
+
             log.info("로그인 성공: {}", userId);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<>(session.getId(),HttpStatus.OK);
         }
         log.warn("로그인 실패: {}", userId);
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
