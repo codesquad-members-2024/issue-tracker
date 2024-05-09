f62807d51975871ae11afe2a83df9e08a0e82a14
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-09 16:30:10 +0900

    refactor log를 Controller에서 service로 옯김

--- src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
@@ -2,14 +2,10 @@ package com.CodeSquad.IssueTracker.user;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 public class UserController {
     private final UserService userService;
@@ -20,11 +16,9 @@ public class UserController {
 
     @PostMapping("/post/registration")
     public ResponseEntity<?> registerNewUser(@RequestBody User user){
-        if (user == null){
-            log.warn("유저를 받아오지 못하였습니다.");
+        if (userService.isUserNotExists(user)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        log.info("새 유저가 회원가입하였습니다.: {}", user);
         userService.save(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -32,25 +26,19 @@ public class UserController {
     @PostMapping("/post/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest,
                                        HttpServletRequest request){
-        if (loginRequest == null){
-            log.warn("로그인 실패: 로그인 데이터가 없습니다.");
+        if (userService.isLoginRequestNotExists(loginRequest)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         String userId = loginRequest.getUserId();
         String userPassword = loginRequest.getUserPassword();
-
-        log.info("로그인 시도: {}", userId);
 
         if (userService.authenticate(userId, userPassword)){
             HttpSession session = request.getSession(true);
             session.setMaxInactiveInterval(1800);
             session.setAttribute("userId", userId);
 
-            log.info("로그인 성공: {}", userId);
             return new ResponseEntity<>(session.getId(),HttpStatus.OK);
         }
-        log.warn("로그인 실패: {}", userId);
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
-
 }
--- src/main/java/com/CodeSquad/IssueTracker/user/UserService.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/UserService.java
@@ -1,11 +1,13 @@
 package com.CodeSquad.IssueTracker.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -22,6 +24,33 @@ public class UserService {
     public boolean authenticate(String userId, String userPassword) {
         Optional<User> userOptional = userRepository.findUserById(userId);
 
-        return userOptional.isPresent() && Objects.equals(userOptional.get().getUserPassword(), userPassword);
+        if (userOptional.isPresent()) {
+            if (Objects.equals(userOptional.get().getUserPassword(), userPassword)){
+                log.info("로그인 성공: {}", userId);
+                return true;
+            }
+        }
+        log.warn("로그인 실패: {}", userId);
+        return false;
     }
+
+    public boolean isUserNotExists(User user) {
+        if (user == null){
+            log.warn("유저를 받아오지 못하였습니다.");
+            return true;
+        }
+        log.info("새 유저가 회원가입하였습니다.: {}", user);
+        return false;
+    }
+
+    public boolean isLoginRequestNotExists(LoginRequest loginRequest) {
+        if (loginRequest == null){
+            log.warn("로그인 실패: 로그인 데이터가 없습니다.");
+            return true;
+        }
+        log.info("로그인 시도");
+        return false;
+    }
+
+
 }
