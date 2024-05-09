a7f6869b2beba47404173acf5cf794ad7eba11da
mi-hye <satge13@gmail.com>
2024-05-09 15:19:30 +0900

    refactor: svg 폴더명 변경

--- fe/src/routes/Login.tsx
+++ fe/src/routes/Login.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as Logo } from "./Logo.svg";
 
 function Login() {
 	const [validId, setValidId] = useState(false);
