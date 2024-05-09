184bd2961933d98f8dea338a5adda61d4553caab
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-09 10:13:21 +0900

    feat 로그인 관련 mapping을 controller에 추가

--- src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
@@ -1,9 +1,13 @@
 package com.CodeSquad.IssueTracker.user;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class UserController {
     private final UserService userService;
@@ -13,12 +17,29 @@ public class UserController {
     }
 
     @PostMapping("/post/registration")
-    public String postRegistration(@RequestBody User user){
+    public ResponseEntity<?> registerNewUser(@RequestBody User user){
+        if (user == null){
+            log.warn("유저를 받아오지 못하였습니다.");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        log.info("새 유저가 회원가입하였습니다.: {}", user);
         userService.save(user);
-        return "success";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @PostMapping("/post/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        String userId = loginRequest.getUserId();
+        String userPassword = loginRequest.getUserPassword();
 
+        log.info("로그인 시도: {}", userId);
 
+        if (userService.authenticate(userId, userPassword)){
+            log.info("로그인 성공: {}", userId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        log.warn("로그인 실패: {}", userId);
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 
 }
