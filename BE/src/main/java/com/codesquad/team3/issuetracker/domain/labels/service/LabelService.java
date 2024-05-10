ab20def1fd92c78a3544fd838a61bc32cbbc2e90
Alex <fnelclsrn123@gmail.com>
2024-05-10 11:25:11 +0900

    feat: db 연결을 위한 spring jdbc template 사용

---
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/service/LabelService.java
@@ -0,0 +1,37 @@
+package com.codesquad.team3.issuetracker.domain.labels.service;
+
+import com.codesquad.team3.issuetracker.domain.labels.dto.Label;
+import com.codesquad.team3.issuetracker.domain.labels.repository.LabelRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class LabelService {
+
+    private final LabelRepository labelRepository;
+
+
+    public LabelService(LabelRepository labelRepository) {
+        this.labelRepository = labelRepository;
+    }
+
+    public void create(Label label){
+        labelRepository.save(label);
+
+    }
+
+    public void update(Long id, Label label){
+        labelRepository.update(id, label);
+    }
+
+    public void delete(Long id){
+        labelRepository.delete(id);
+    }
+
+
+    public Label getLabel(Long id) {
+        return labelRepository.get(id);
+
+
+    }
+}
