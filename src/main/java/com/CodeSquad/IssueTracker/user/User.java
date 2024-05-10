88e761e988ec15a8e43c46a7dca8de05b84e1ec5
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-10 10:02:19 +0900

    refactor User에서 겹치는 어노테이션 제거

--- src/main/java/com/CodeSquad/IssueTracker/user/User.java
+++ src/main/java/com/CodeSquad/IssueTracker/user/User.java
@@ -1,10 +1,8 @@
 package com.CodeSquad.IssueTracker.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class User {
     private String userId;
     private String userPassword;
