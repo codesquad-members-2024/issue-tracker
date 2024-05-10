82a0ee63faf43af5dc8bec1ddb8c1c48ef9d8665
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-05-10 20:39:17 +0900

    [feat/#8] fix: 잘못된 RequestMapping URL 수정

--- src/main/java/com/issuetracker/domain/issue/IssueController.java
+++ src/main/java/com/issuetracker/domain/issue/IssueController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/api/vi/issues")
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
