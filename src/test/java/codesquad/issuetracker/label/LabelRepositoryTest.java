9c67ad05f1d96a62454d1e1009694c667963ba49
hiidy <hiidy115@gmail.com>
2024-05-10 19:20:33 +0900

    :sparkles: feat: 모든 라벨을 fetch하는 기능 구현
    - 모든 라벨을 fetch를 위한 controller, repository, service의 테스트 코드를 구현
    - label entity에 name 필드 추가

---
+++ src/test/java/codesquad/issuetracker/label/LabelRepositoryTest.java
@@ -0,0 +1,41 @@
+package codesquad.issuetracker.label;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+
+@DataJdbcTest
+class LabelRepositoryTest {
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Test
+    @DisplayName("테스트 라벨 객체를 만들고 저장하는 테스트")
+    void testLabelSave() {
+
+        Label label = Label.builder()
+            .name("테스트 라벨")
+            .description("테스트 내용")
+            .color("#FF0000")
+            .build();
+
+        Label savedLabel = labelRepository.save(label);
+
+
+        Optional<Label> fetchedLabelOpt = labelRepository.findById(
+            savedLabel.getId());
+
+        assertThat(fetchedLabelOpt).isPresent();
+
+        Label fetchedLabel = fetchedLabelOpt.get();
+        assertThat(fetchedLabel).usingRecursiveComparison().ignoringFields("id")
+            .isEqualTo(savedLabel);
+
+    }
+
+}
