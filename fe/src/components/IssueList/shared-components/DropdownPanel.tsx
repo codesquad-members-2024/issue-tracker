f8c73bdbe066f9dae3348da30a421a4c1b057528
mi-hye <satge13@gmail.com>
2024-05-10 10:28:36 +0900

    feat: 드롭패널 공통 컴포넌트 구현

---
+++ fe/src/components/IssueList/shared-components/DropdownPanel.tsx
@@ -0,0 +1,59 @@
+const borderBottom = "border-b-2 border-grayscale.300 dark:border-grayscale.600";
+function DropdownPanel() {
+	//TODO : 이하 변수들은 차후 props로 변경 예정
+	const w = "w-[240px]";
+	const filterTitle = "이슈 필터";
+	const imgs: any = [
+		// "https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__",
+		// "https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__",
+		// "https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__",
+		// "https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__",
+		// "https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__",
+	];
+	const contents = [
+		"열린 이슈",
+		"내가 작성한 이슈",
+		"나에게 할당된 이슈",
+		"내가 댓글을 남긴 이슈",
+		"닫힌 이슈",
+	];
+	//
+
+	return (
+		<div className={`absolute top-12 ${w} component-border dark:component-border--dark`}>
+			<h3 className={`${borderBottom} flex items-center cursor-default`}>
+				<span className="mx-3 my-2 text-xs text-grayscale.600 dark:text-grayscale.500">
+					{filterTitle}
+				</span>
+			</h3>
+			<ul>
+				{contents.map((content, i) => (
+					<li
+						key={i}
+						className={`py-2 flex items-center justify-between w-full bg-grayscale.50 dark:bg-grayscale.800 ${
+							i === contents.length - 1 ? " rounded-b-xl" : borderBottom
+						}`}
+					>
+						<div className="flex items-center">
+							{imgs.length !== 0 && (
+								<img
+									className="w-[20px] h-[20px] rounded-full ml-3"
+									alt="userProfile"
+									src={imgs[i]}
+								/>
+							)}
+							<span className="ml-2 text-grayscale.700 dark:text-grayscale.400">{content}</span>
+						</div>
+						<input className="hidden peer" type="checkbox" id={content} />
+						<label
+							className="mx-3 checkbox dark:border-grayscale.400 peer-checked:after:checkbox--checked dark:peer-checked:after:checkbox--checked--dark"
+							htmlFor={content}
+						></label>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default DropdownPanel;
