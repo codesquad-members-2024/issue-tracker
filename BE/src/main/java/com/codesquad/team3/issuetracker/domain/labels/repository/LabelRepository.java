de51c94af2e57fdc712a9da844d63d54689621fc
Alex <fnelclsrn123@gmail.com>
2024-05-10 11:25:11 +0900

    feat: db 연결을 위한 spring jdbc template 사용

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/repository/LabelRepository.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/repository/LabelRepository.java
@@ -2,7 +2,11 @@ package com.codesquad.team3.issuetracker.domain.labels.repository;
 
 import com.codesquad.team3.issuetracker.domain.labels.dto.Label;
 
-public interface Repository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LabelRepository extends CrudRepository<Label, Long> {
 
 
     public Label save(Label label);
--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/repository/MemoryLabelRepository.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/repository/MemoryLabelRepository.java
@@ -6,21 +6,19 @@ import com.codesquad.team3.issuetracker.domain.labels.dto.Label;
 import java.util.HashMap;
 import java.util.Map;
 
-@org.springframework.stereotype.Repository
-public class MemoryRepository implements Repository {
+public class MemoryLabelRepository {
 
 
     private final Map<Long, Label> repo = new HashMap<>();
     private Long sequence=0L;
 
 
-    @Override
+
     public Label save(Label label) {
         repo.put(++sequence, label);
         return label;
     }
 
-    @Override
     public Label update(Long id, Label label) {
         repo.remove(id);
         repo.put(id, label);
@@ -28,12 +26,10 @@ public class MemoryRepository implements Repository {
         return label;
     }
 
-    @Override
     public void delete(Long id) {
      repo.remove(id);
     }
 
-    @Override
     public Label get(Long id) {
         return repo.get(id);
     }
