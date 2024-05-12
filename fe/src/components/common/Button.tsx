87592339976805ff71e29717a7f9e73578599a66
mi-hye <satge13@gmail.com>
2024-05-12 22:22:23 +0900

    feat: 버튼 컴포넌트 구현

--- fe/src/components/common/Button.tsx
+++ fe/src/components/common/Button.tsx
@@ -1,17 +1,68 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {
+	faPlus,
+	faX,
+	faCircleExclamation,
+	faBoxArchive,
+	faPaperclip,
+} from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare, faFaceSmile, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
-function Button() {
-	const small = "w-[128px] h-[40px]";
-	const container = "bg-grayscale.700 text-grayscale.50 drak:";
-	const icon = faPlus;
+interface PropsType {
+	size: string;
+	type: string;
+	icon: string | null;
+	text: string;
+	state: string;
+}
+interface DesignMap {
+	[key: string]: string;
+}
+interface IconType {
+	[key: string]: IconProp;
+}
 
+const SIZE: DesignMap = {
+	S: "w-[128px] h-[40px] text-xs",
+	M: "w-[184px] h-[48px]",
+	L: "w-[240px] h-[56px] text-xl",
+};
+const SIZE_PADDING: DesignMap = {
+	S: "px-[4px]",
+	M: "px-[8px]",
+	L: "px-[8px]",
+};
+const ICON: IconType = {
+	X: faX,
+	PLUS: faPlus,
+	"!": faCircleExclamation,
+	ARCHIVE: faBoxArchive,
+	CLIP: faPaperclip,
+	SMLILE: faFaceSmile,
+	PEN: faPenToSquare,
+	TRASH: faTrashCan,
+};
+const TYPE: DesignMap = {
+	CONTAINED: "bg-grayscale.700 text-grayscale.50",
+	OUTLINE:
+		"border-[1px] border-grayscale.700 text-grayscale.700 dark:text-grayscale.400 dark:border-grayscale.400",
+	GHOST: "text-grayscale.700 dark:text-grayscale.400",
+	GHOST_RED: "text-accent.red",
+};
+const STATE: DesignMap = {
+	SELECTED: "font-bold",
+	DEFAULT: "hover:opacity-80",
+	DISABLED: "opacity-[32%] cursor-default",
+};
+
+function Button({ size, type, icon, text, state }: PropsType) {
 	return (
 		<button
-			className={`${small} ${container} relative flex items-center justify-center rounded-xl text-xs`}
+			className={`${SIZE[size]} ${TYPE[type]} ${STATE[state]} relative flex items-center justify-center rounded-xl`}
 		>
-			<FontAwesomeIcon className="mt-[1.4px] text-base" icon={icon} />
-			<span className="ml-2">이슈 작성</span>
+			{icon && <FontAwesomeIcon className="mt-[1.4px]" icon={ICON[icon]} />}
+			<span className={SIZE_PADDING[size]}>{text}</span>
 		</button>
 	);
 }
