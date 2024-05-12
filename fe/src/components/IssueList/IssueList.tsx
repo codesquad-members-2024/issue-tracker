d37eb04cf263402fc0b2e844821f216776035e86
mi-hye <satge13@gmail.com>
2024-05-13 00:35:18 +0900

    feat: Table 컴포넌트 구현

--- fe/src/components/IssueList/IssueList.tsx
+++ fe/src/components/IssueList/IssueList.tsx
@@ -1,9 +1,11 @@
+import Table from "../common/Table";
 import UIBar from "./UIBar/UIBar";
 
 function IssueList() {
 	return (
-		<div className="flex justify-center">
+		<div className="flex flex-col justify-center">
 			<UIBar />
+			<Table />
 		</div>
 	);
 }
---
+++ fe/src/components/common/Table.tsx
@@ -0,0 +1,5 @@
+function Table() {
+	return <div className=" bg-sky-200">테이블임당</div>;
+}
+
+export default Table;
