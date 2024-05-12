6a241abf8c02959fa1157c1be48720ac7de8fde1
mi-hye <satge13@gmail.com>
2024-05-12 22:23:10 +0900

    feat: 이슈 작성 버튼 추가

--- fe/src/components/IssueList/UIBar/UIBar.tsx
+++ fe/src/components/IssueList/UIBar/UIBar.tsx
@@ -1,14 +1,14 @@
 import Button from "../../common/Button";
 import Filter from "./Filter";
-import TabButton from "./TabButton";
+import TabButton from "../../common/TabButton";
 
 function UI() {
 	return (
 		<div className="container mx-lg my-16 flex justify-between h-[40px]">
 			<Filter />
-			<div className="flex h-full w-2/5 bg-sky-100">
+			<div className="flex h-full w-2/5 ">
 				<TabButton />
-				<Button />
+				<Button size="S" type="CONTAINED" icon="PLUS" text="이슈 작성" state="DEFAULT" />
 			</div>
 		</div>
 	);
