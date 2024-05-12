75d966ebd536ad40bffc205f688eb97d157e8cd6
melroh629 <jw.roh629@gmail.com>
2024-05-12 22:50:59 +0900

    feat: 아이콘 추가(plus,smile)

---
+++ front/src/common/icons/index.js
@@ -0,0 +1,7 @@
+export * from './IconAlertCircle';
+export * from './IconEdit';
+export * from './IconTrash';
+export * from './IconUser';
+export * from './IconLandmark';
+export * from './IconSmile';
+export * from './IconPlus';
