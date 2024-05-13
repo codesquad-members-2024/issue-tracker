e15f7e17bc0e5538b37f8b70f09082e44be92217
Daniel <923daniel@naver.com>
2024-05-13 11:12:00 +0900

    feat: user_manager 테이블에 대응하는 UserManager 클래스 구현

---
+++ back/src/main/java/codesquad/issuetracker/domain/UserManager.java
@@ -0,0 +1,15 @@
+package codesquad.issuetracker.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Table("user_manager")
+public class UserManager {
+
+    @Id
+    private Long id;
+    private String user_id;
+    private Long issue_id;
+}
