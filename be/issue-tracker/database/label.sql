d1262222bad0ec7fad68f28f20164ece3cea3183
growth-mango <esudatcom@gmail.com>
2024-05-13 18:23:06 +0900

    feat: label table DDL 작성

---
+++ be/issue-tracker/database/label.sql
@@ -0,0 +1,7 @@
+CREATE TABLE label (
+                       id BIGINT AUTO_INCREMENT primary key,
+                       name VARCHAR(255),
+                       description VARCHAR(255),
+                       background_color VARCHAR(16),
+                       text_color VARCHAR(16)
+);
