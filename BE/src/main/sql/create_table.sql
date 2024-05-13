bf83c44023fbdaece1744856932fbb3fddcd1441
kimjooyoung <kjy915875@gmail.com>
2024-05-13 15:38:06 +0900

    feat. varchar pk도 insert가능하게 수정

--- BE/src/main/sql/create_table.sql
+++ BE/src/main/sql/create_table.sql
@@ -1,23 +1,22 @@
 CREATE TABLE issue (
-                       id int PRIMARY KEY,
+                       id int PRIMARY KEY AUTO_INCREMENT,
                        writer_name varchar(20) NOT NULL UNIQUE ,
                        title varchar(60) NOT NULL,
-                       contents text NOT NULL,
-                       createTime timestamp NOT NULL,
-                       isClose bool DEFAULT false,
+                       create_time timestamp NOT NULL,
+                       is_close bool DEFAULT false,
                        writer_id varchar(16) NOT NULL UNIQUE ,
                        milestone_title varchar(20),
-                       isDeleted bool DEFAULT false
+                       is_deleted bool DEFAULT false
 );
 
 CREATE TABLE comment (
-                         id int PRIMARY KEY,
+                         id int PRIMARY KEY AUTO_INCREMENT,
                          writer_id varchar(16) NOT NULL UNIQUE ,
                          writer_name varchar(20) NOT NULL UNIQUE ,
                          contents varchar(500) NOT NULL,
-                         createTime timestamp NOT NULL,
+                         create_time timestamp NOT NULL,
                          issue_id int NOT NULL UNIQUE ,
-                         isDeleted bool DEFAULT false
+                         is_delete bool DEFAULT false
 );
 
 CREATE TABLE member (
@@ -26,9 +25,9 @@ CREATE TABLE member (
                         nickname varchar(20) NOT NULL UNIQUE ,
                         profile_img varchar(50),
                         birthday timestamp NOT NULL,
-                        joinTime timestamp NOT NULL,
+                        join_time timestamp NOT NULL,
                         email varchar(50) NOT NULL,
-                        isDeleted bool DEFAULT false
+                        is_delete bool DEFAULT false
 );
 
 CREATE TABLE label (
@@ -42,21 +41,15 @@ CREATE TABLE label (
 CREATE TABLE milestone (
                            title varchar(20) PRIMARY KEY,
                            description varchar(100),
-                           deadLine timestamp,
-                           isClosed bool DEFAULT false,
-                           isDeleted bool DEFAULT false
+                           deadline timestamp,
+                           is_close bool DEFAULT false,
+                           is_deleted bool DEFAULT false
 );
 
 CREATE TABLE labels_in_issue (
                                  label_title varchar(10) NOT NULL,
                                  issue_id int NOT NULL,
                                  PRIMARY KEY (label_title, issue_id)
-);
-
-CREATE TABLE uploaded_file_in_issue (
-                                        file varchar(50),
-                                        issue_id int,
-                                        PRIMARY KEY (file, issue_id)
 );
 
 CREATE TABLE uploaded_file_in_comment (
@@ -74,8 +67,6 @@ CREATE TABLE assigner(
 
 ALTER TABLE assigner ADD FOREIGN KEY (issue_id) REFERENCES issue(id);
 ALTER TABLE assigner ADD FOREIGN KEY (assigner_id) REFERENCES member(id);
-
-ALTER TABLE uploaded_file_in_issue ADD FOREIGN KEY (issue_id) REFERENCES issue (id);
 
 ALTER TABLE uploaded_file_in_comment ADD FOREIGN KEY (comment_id) REFERENCES comment (id);
 
