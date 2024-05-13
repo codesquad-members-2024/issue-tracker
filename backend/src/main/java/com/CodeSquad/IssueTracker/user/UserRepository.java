89de57df82f9c8d5c683fe87be69ef67be785fb1
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-13 16:13:39 +0900

    feature Crud Repository 사용

--- backend/src/main/java/com/CodeSquad/IssueTracker/user/UserRepository.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/user/UserRepository.java
@@ -1,36 +1,8 @@
 package com.CodeSquad.IssueTracker.user;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public class UserRepository {
-    JdbcTemplate jdbcTemplate;
-
-    public UserRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public void save(User user) {
-        String sql = "insert into users (userId, userPassword, userNickname) values (?, ?, ?)";
-        jdbcTemplate.update(sql,user.getUserId(),user.getUserPassword(),user.getUserNickname());
-    }
-
-    public Optional<User> findUserById(String userId) {
-        String sql = "SELECT * FROM users WHERE userId = ?";
-        List<User> user = jdbcTemplate.query(sql, userRowMapper(), userId);
-        return user.stream().findAny();
-    }
-
-    private RowMapper<User> userRowMapper() {
-        return (rs, rowNum) -> new User(
-                rs.getString("userId"),
-                rs.getString("userPassword"),
-                rs.getString("userNickname")
-        );
-    }
+public interface UserRepository extends CrudRepository<User, String> {
 }
