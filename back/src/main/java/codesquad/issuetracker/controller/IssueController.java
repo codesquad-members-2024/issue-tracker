897457115491f5e2e0a1387c7b31d9011df3c63b
Daniel <923daniel@naver.com>
2024-05-13 11:05:28 +0900

    chore: cors 오류 해결 위한 CORS 설정 추가

--- back/src/main/java/codesquad/issuetracker/controller/IssueController.java
+++ back/src/main/java/codesquad/issuetracker/controller/IssueController.java
@@ -3,13 +3,11 @@ package codesquad.issuetracker.controller;
 import codesquad.issuetracker.domain.Issue;
 import codesquad.issuetracker.service.IssueService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @Controller
 public class IssueController {
 
