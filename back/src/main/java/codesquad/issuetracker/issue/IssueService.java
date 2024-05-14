53808920753c987e96a1b0660efdd2c187cb6177
yonghyeonpark <pyh4616@naver.com>
2024-05-14 10:07:58 +0900

    refactor: IssueService 메서드 수정
    - Dto를controller에서 생성하기 위한 메서드 수정
    - 반환 타입을 Dto가 아닌 엔터티 클래스로 수정
    - Dto 객체 생성을 위한 메서드들의 접근제어자를 public으로 수정

--- back/src/main/java/codesquad/issuetracker/issue/IssueService.java
+++ back/src/main/java/codesquad/issuetracker/issue/IssueService.java
@@ -17,23 +17,21 @@ public class IssueService {
     private final LabelRepository labelRepository;
     private final UserRepository userRepository;
 
-    // IssueShowDto 만드는 매소드를 서비스에 구현한 이유
-    // IssueRepository issueRepository; 를 컨트롤러에도 추가하는게 어색해서 일단 여기에 구현했습니다.
-    // 근데 잘 모르겠어서 컨트롤러로 옮겨도 넘 좋아요!!!
-    public List<IssueShowDto> getAllIssues() {
-        List<Issue> issues = (List<Issue>) issueRepository.findAll();
-        return issues.stream().map(issue -> new IssueShowDto(issue.getId(), issue.getTitle(), issue.getContent(), issue.getMilestoneId(), getIssueAssignees(issue), issue.getWriter(), issue.getCreateTime(), issue.getIsClosed(), getIssueLabels(issue))).toList();
+    public List<Issue> getAllIssues() {
+        return (List<Issue>) issueRepository.findAll();
     }
 
-    // 중간 테이블 IssueLabel를 이용해, issue에 있는 Label를 List<Label>로 만들어 반환
-    private List<Label> getIssueLabels(Issue issue) {
-        List<String> labelNames = issue.getLabels().stream().map(IssueLabel::getLabelName).toList();
+    public List<Label> getLabelsForIssue(Issue issue) {
+        List<String> labelNames = issue.getLabels().stream()
+                .map(IssueLabel::getLabelName)
+                .toList();
         return (List<Label>) labelRepository.findAllById(labelNames);
     }
 
-    // 중간 테이블 IssueAssignee 이용해, issue에 있는 Assignee를 List<User>로 만들어 반환
-    private List<User> getIssueAssignees(Issue issue) {
-        List<String> assigneeNames = issue.getAssignees().stream().map(IssueAssignee::getUserLoginId).toList();
+    public List<User> getAssigneesForIssue(Issue issue) {
+        List<String> assigneeNames = issue.getAssignees().stream()
+                .map(IssueAssignee::getUserLoginId)
+                .toList();
         return (List<User>) userRepository.findAllById(assigneeNames);
     }
 }
