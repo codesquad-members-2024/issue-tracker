ec5d8bc5329b866e32f3449d79e8376f6657965a
jueun-park98 <jooeun06161@gmail.com>
2024-05-14 17:22:57 +0900

    feat: 이슈 헤드라인 및 열린 이슈, 닫힌 이슈 클릭 핸들링 추가

--- frontend/src/components/list/IssueList.tsx
+++ frontend/src/components/list/IssueList.tsx
@@ -1,23 +1,46 @@
 import styled from "styled-components";
 import IssueTab from "./IssueTab";
 import IssueHeadline from "./IssueHeadline";
+import { useMutation } from "react-query";
+import { sendIssuesRequest } from "../../api/IssueAPI";
+import { useEffect, useState } from "react";
+import useIssueStore from "../../hooks/useIssueStore";
+
+export type IssueType = "open" | "closed";
 
 function IssueList() {
+  const { issues, setIssues } = useIssueStore();
+  const [focusedTab, setFocusedTab] = useState<IssueType>("open"); // deprecated after the completion of api
+  const { mutate: fetchIssues } = useMutation(sendIssuesRequest, {
+    onSuccess: (data) => setIssues(data),
+  });
+
+  useEffect(() => fetchIssues(), []);
+
   return (
     <Wrapper>
-      <IssueTab />
-      <IssueHeadline />
-      <IssueHeadline />
+      <IssueTab focusedTab={focusedTab} setFocusedTab={setFocusedTab}/>
+      {issues
+        .filter(({ isClosed }) => focusedTab === "open" ? !isClosed : isClosed)
+        .map(({ id, title, author, publishedAt }) => (
+          <IssueHeadline issueId={id} title={title} author={author} publishedAt={publishedAt} />
+        ))}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   width: 1280px;
+  height: 55em;
   margin-top: 1.5em;
-  border: 1px solid #D9DBE9;
+  border: 1px solid #d9dbe9;
   border-radius: 0.725em;
   overflow: hidden;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default IssueList;
