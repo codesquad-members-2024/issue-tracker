d91820c4c28a18e680b586792a3392b2183ec071
Alex <fnelclsrn123@gmail.com>
2024-05-14 17:20:48 +0900

    feat: labelfrom 유효성 검사
    -validation으로 공백, 빈칸 유효성 검사

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/dto/request/LabelForm.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/dto/request/LabelForm.java
@@ -1,14 +1,20 @@
 package com.codesquad.team3.issuetracker.domain.labels.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 @AllArgsConstructor
 @Getter
 public class LabelForm {
 
+    @NotBlank(message = "이름은 필수 입력값입니다.")
+    @Length(max = 10)
     private final String title;
+    @NotBlank(message ="설명은 필수 입력값입니다.")
     private final String description;
+    @NotBlank(message ="색은 필수 입력값입니다.")
     private final String color;
 
 }
