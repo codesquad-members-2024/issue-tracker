7bdae80a85ba4147931f7364ffb1ae9536c49212
Alex <fnelclsrn123@gmail.com>
2024-05-14 17:24:23 +0900

    refactor: update 메서드 제거

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/repository/LabelRepository.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/repository/LabelRepository.java
@@ -1,24 +1,10 @@
 package com.codesquad.team3.issuetracker.domain.labels.repository;
 
 import com.codesquad.team3.issuetracker.domain.labels.entity.Label;
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import com.codesquad.team3.issuetracker.support.repository.GlobalCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LabelRepository extends CrudRepository<Label, String> {
-
-    @Modifying
-    @Query("UPDATE LABEL SET title =:newTitle, " +
-            "description =:description, " +
-            "color =:color where title =:title")
-    void updateByTitle(
-            @Param("title") String title,
-            @Param("description") String description,
-            @Param("color") String color,
-            @Param("newTitle") String newTitle);
-
+public interface LabelRepository extends GlobalCrudRepository<Label, String> {
 
 }
