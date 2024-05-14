a2ba4aadcd77809d89b8e6460bfe625bc650be87
yonghyeonpark <pyh4616@naver.com>
2024-05-14 16:51:50 +0900

    refactor: 불필요한 메서드 삭제

--- back/src/main/java/codesquad/issuetracker/issue/IssueRepository.java
+++ back/src/main/java/codesquad/issuetracker/issue/IssueRepository.java
@@ -1,12 +1,6 @@
 package codesquad.issuetracker.issue;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface IssueRepository extends CrudRepository<Issue, Long> {
-
-    @Query("SELECT * FROM issue")
-    List<Issue> findAllIssues();
 }
