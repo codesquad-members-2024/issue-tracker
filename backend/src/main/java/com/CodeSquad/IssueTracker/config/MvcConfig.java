310e8c66c75ab8f87112008be115cf544c31d2a7
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-14 10:04:34 +0900

    feature CORS 세팅 추가

--- backend/src/main/java/com/CodeSquad/IssueTracker/config/MvcConfig.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.CodeSquad.IssueTracker.config;
 
 import com.CodeSquad.IssueTracker.interceptor.CheckLoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,17 @@ public class MvcConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/issue/**", "/issues")
                 .excludePathPatterns("/login", "/registration");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://d1h2zmtghg8ybi.cloudfront.net",
+                        "http://115.90.185.254"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
--- backend/src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/user/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*") //나중에 변경
 @RestController
 public class UserController {
     private final UserService userService;
