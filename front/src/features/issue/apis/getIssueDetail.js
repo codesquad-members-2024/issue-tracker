1ea4b64a653fa777019eac980d82cba64c4b1518
melroh629 <jw.roh629@gmail.com>
2024-05-14 15:44:42 +0900

    fix: api request path 변경

--- front/src/features/issue/apis/getIssueDetail.js
+++ front/src/features/issue/apis/getIssueDetail.js
@@ -1,17 +1,18 @@
-import { server, devServer } from '../../../apis/baseApi';
+import { server, devServer } from '~/apis/baseApi';
 
 /**
  * 이슈 상세 조회 API
+ * @url http://3.38.30.88:8080/issues/{id}
  * @param {number} id : 이슈 id
  * @returns {object} : 이슈 상세 정보
  */
 export async function getIssueDetail(id) {
-	console.log(`id: ${id}`);
 	try {
-		const response = await fetch(`${devServer}/issue?id=${id}`).then(res =>
+		const response = await fetch(`${server}/issues/${id}`).then(res =>
 			res.json()
 		);
-		const data = await response;
+		// console.log('⛳️ success getIssueDetail', response);
+		const data = response;
 		return data;
 	} catch (error) {
 		console.error(error);
--- front/src/features/issue/apis/getIssues.js
+++ front/src/features/issue/apis/getIssues.js
@@ -1,8 +1,13 @@
 import { server, devServer } from '../../../apis/baseApi';
 
+/**
+ * 이슈 목록 조회 API
+ * @url http://3.38.30.88:8080/issues
+ * @returns {object} : 이슈 리스트 정보
+ */
 export async function getIssues() {
 	try {
-		const response = await fetch(`${devServer}/issue`).then(res => res.json());
+		const response = await fetch(`${server}/issues`).then(res => res.json());
 		const data = await response;
 		return data;
 	} catch (error) {
