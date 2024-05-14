f0f3d0b3aaf9c424e2f0013760052d4bd5fc898b
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-05-14 18:14:38 +0900

    [feat/#16] feat: data.sql 충돌 해결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@
 -- test user insert
 INSERT INTO MEMBER(MEMBER_ID, PASSWORD) VALUES ("tester", "1234");
 
-INSERT INTO ISSUE (MEMBER_ID, TITLE, CONTENT, IS_OPEN) VALUES ("tester", "test", "test zz", true);
-
 INSERT INTO MILESTONE(NAME) VALUES("testMilestone");
+
+INSERT INTO ISSUE(MEMBER_ID, TITLE, CONTENT) VALUES ("tester", "testTitle", "testContent");
