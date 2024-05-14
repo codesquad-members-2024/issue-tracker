afb371c58a3511dcb50cb72a9eb8cfbf2af4113d
Alex <fnelclsrn123@gmail.com>
2024-05-14 17:31:12 +0900

    feat: 마일스톤 엔티티를 위한 클래스 생성
    -엔티티, 폼 클래스 생성

---
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/milestone/dto/request/MilestoneForm.java
@@ -0,0 +1,23 @@
+package com.codesquad.team3.issuetracker.domain.milestone.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
+public class MilestoneForm {
+
+    @NotBlank(message = "제목은 필수 입력값입니다.")
+    private String title;
+
+    @NotBlank(message = "설명은 필수 입력값입니다.")
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate deadLine;
+
+}
---
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/milestone/entity/Milestone.java
@@ -0,0 +1,21 @@
+package com.codesquad.team3.issuetracker.domain.milestone.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+public class Milestone {
+
+    @Id
+    private final String title;
+    private final String description;
+    private final LocalDate deadline;
+
+    private boolean isClosed;
+    private boolean isDeleted;
+
+}
