cae9c6a3ff49e187fbe57734230a921c06abb380
Daniel <923daniel@naver.com>
2024-05-14 19:10:23 +0900

    refactor: calculateTimeDifference 매소드에 weeks 반환 기능 수정.

--- back/src/main/java/codesquad/issuetracker/util/TimeCalculator.java
+++ back/src/main/java/codesquad/issuetracker/util/TimeCalculator.java
@@ -14,10 +14,13 @@ public class TimeCalculator {
         long minutes = seconds / 60;
         long hours = minutes / 60;
         long days = hours / 24;
+        long weeks = days / 7;
         long years = days / 365;
 
         if (years > 0) {
             return years + "년";
+        } else if (weeks > 0) {
+            return weeks + "주";
         } else if (days > 0) {
             return days + "일";
         } else if (hours > 0) {
