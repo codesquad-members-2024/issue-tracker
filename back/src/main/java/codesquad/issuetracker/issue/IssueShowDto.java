f60661997036a17d168cd50df31847eb5cded01f
yonghyeonpark <pyh4616@naver.com>
2024-05-14 10:05:39 +0900

    refactor: issueShowDto 파라미터 수정
    - 데이터를 직접 넘겨 받지 않고, issue 객체를 받도록 수정
    - dto 객체 생성을 위한 파라미터 인자 추가(labels, assignees)

--- back/src/main/java/codesquad/issuetracker/issue/IssueShowDto.java
+++ back/src/main/java/codesquad/issuetracker/issue/IssueShowDto.java
@@ -1,23 +1,40 @@
 package codesquad.issuetracker.issue;
 
 import codesquad.issuetracker.label.Label;
+import codesquad.issuetracker.label.LabelShowDto;
+import codesquad.issuetracker.user.AssigneeShowDto;
 import codesquad.issuetracker.user.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class IssueShowDto {
     private Long id;
     private String title;
     private String content;
     private String milestoneId;
-    private List<User> assignees;
+    private List<AssigneeShowDto> assignees;
     private String writer;
     private LocalDateTime createTime;
     private boolean isClosed; // 기본 값 false
-    private List<Label> labels;
+    private List<LabelShowDto> labels;
+
+    public IssueShowDto(Issue issue, List<Label> labels, List<User> assignees) {
+        this.id = issue.getId();
+        this.title = issue.getTitle();
+        this.content = issue.getContent();
+        this.milestoneId = issue.getMilestoneId();
+        this.assignees = assignees.stream()
+                .map(user -> new AssigneeShowDto(user))
+                .collect(Collectors.toList());
+        this.writer = issue.getWriter();
+        this.createTime = issue.getCreateTime();
+        this.isClosed = issue.getIsClosed();
+        this.labels = labels.stream()
+                .map(label -> new LabelShowDto(label))
+                .collect(Collectors.toList());
+    }
 }
