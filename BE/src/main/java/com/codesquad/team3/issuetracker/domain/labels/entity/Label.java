6529ab09134188f574102979f93e32729b85f54c
Alex <fnelclsrn123@gmail.com>
2024-05-14 17:23:03 +0900

    feat: label 클래스 변경
    -setter 추가

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/entity/Label.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/labels/entity/Label.java
@@ -2,10 +2,12 @@ package com.codesquad.team3.issuetracker.domain.labels.entity;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
 public class Label {
 
@@ -13,6 +15,7 @@ public class Label {
     private final String title;
     private final String description;
     private final String color;
+
     private final LocalDateTime createTime;
     private boolean isDeleted;
 
