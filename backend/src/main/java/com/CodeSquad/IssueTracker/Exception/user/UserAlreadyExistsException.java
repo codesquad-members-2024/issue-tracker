bf171b64fee1eed630af8ef319ce7db79af97088
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-15 18:21:29 +0900

    refactor 안쓰이는 커스텀 에러 삭제

--- backend/src/main/java/com/CodeSquad/IssueTracker/Exception/user/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.CodeSquad.IssueTracker.Exception.user;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
