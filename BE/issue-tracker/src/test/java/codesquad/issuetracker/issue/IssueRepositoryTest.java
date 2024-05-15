d99f08ad21f9d59c5306d085464e3c14c8e36462
Keon Kim <fnc6488@gmail.com>
2024-05-15 16:00:15 +0900

    :recycle: refactor: Issue 테스트 코드 리팩터링
    - 연관 데이터를 @Sql 애노테이션으로 미리 넣어두도록 변경
    - Label과 관련된 테스트 메서드 assert 문 추가

--- BE/issue-tracker/src/test/java/codesquad/issuetracker/issue/IssueRepositoryTest.java
+++ BE/issue-tracker/src/test/java/codesquad/issuetracker/issue/IssueRepositoryTest.java
@@ -2,14 +2,10 @@ package codesquad.issuetracker.issue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import codesquad.issuetracker.user.User;
-import codesquad.issuetracker.user.User.Role;
-import codesquad.issuetracker.user.UserRepository;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +14,11 @@ import org.springframework.test.context.jdbc.Sql;
 
 @DataJdbcTest
 @Slf4j
+@Sql({"/sql/label-data.sql", "/sql/user-data.sql"})
 class IssueRepositoryTest {
 
     @Autowired
     IssueRepository issueRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @BeforeEach
-    void init() {
-        User user = User.from("cori1234", "cori", "123", Role.User);
-        userRepository.save(user);
-    }
 
     @Test
     @DisplayName("열린 이슈 목록 가져오기")
@@ -61,18 +49,17 @@ class IssueRepositoryTest {
 
     @Test
     @DisplayName("레이블 레퍼런스를 가져올 수 있다.")
-    @Sql("/sql/label-data.sql")
     void findLabels() {
         Set<LabelRef> labelRefs = Set.of(new LabelRef(1L), new LabelRef(2L), new LabelRef(3L));
         Issue issue = Issue.from("cori1234", "제목", "내용", 1L, labelRefs);
         Issue savedIssued = issueRepository.save(issue);
 
-        log.info("labels = {}", savedIssued.getLabelRefs());
+        Issue findIssue = issueRepository.findById(savedIssued.getId()).get();
+        assertThat(findIssue.getLabelRefs()).hasSize(labelRefs.size());
     }
 
     @Test
     @DisplayName("해당 레이블을 가지고 있는 이슈를 가져올 수 있다.")
-    @Sql("/sql/label-data.sql")
     void findIssuesByLabel() {
         LabelRef labelRef1 = new LabelRef(1L);
         LabelRef labelRef2 = new LabelRef(2L);
@@ -81,12 +68,12 @@ class IssueRepositoryTest {
         Issue issue1 = Issue.from("cori1234", "제목1", "내용", 1L, Set.of(labelRef3));
         Issue issue2 = Issue.from("cori1234", "제목2", "내용", 1L, Set.of(labelRef2));
         Issue issue3 = Issue.from("cori1234", "제목3", "내용", 1L, Set.of(labelRef1));
-        Issue issue4 = Issue.from("cori1234", "제목4", "내용", 1L, Set.of(labelRef1));
 
         issueRepository.save(issue1);
         issueRepository.save(issue2);
         issueRepository.save(issue3);
 
-        log.info("issues = {}", issueRepository.findAllByLabelId(1L));
+        List<Issue> issues = issueRepository.findAllByLabelId(1L);
+        assertThat(issues).allMatch(issue -> issue.getLabelRefs().contains(labelRef1));
     }
 }
