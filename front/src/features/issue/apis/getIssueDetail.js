fab1a15c1cf3ebfd638e277c0ec8b74b5e46d53d
melroh629 <jw.roh629@gmail.com>
2024-05-15 21:56:02 +0900

    chore: json-server 경로 설정을 위해 분리

--- front/src/features/issue/apis/getIssueDetail.js
+++ front/src/features/issue/apis/getIssueDetail.js
@@ -6,12 +6,12 @@ import { server, devServer } from '~/apis/baseApi';
  * @param {number} id : 이슈 id
  * @returns {object} : 이슈 상세 정보
  */
+const prod = `${server}/detail?id=1`;
+const dev = `${devServer}/detail?id=1`;
 export async function getIssueDetail(id) {
 	try {
-		const response = await fetch(`${server}/issues/${id}`).then(res =>
-			res.json()
-		);
-		// console.log('⛳️ success getIssueDetail', response);
+		const response = await fetch(dev).then(res => res.json());
+
 		const data = response;
 		return data;
 	} catch (error) {
@@ -19,3 +19,39 @@ export async function getIssueDetail(id) {
 		return error;
 	}
 }
+
+/**
+ * Response Example
+{
+  "id": 1,
+  "title": "title1",
+  "content": "my name is daniel",
+  "milestoneId": "m1",
+  "assignees": [
+    {
+      "loginId": "mellisa",
+      "profileImage": "https://avatars.githubusercontent.com/u/140429591?s=40&v=4"
+    },
+    {
+      "loginId": "wade",
+      "profileImage": "https://avatars.githubusercontent.com/u/126778700?s=40&v=4"
+    }
+  ],
+  "writer": "daniel",
+  "createTime": "2024-05-14T04:41:45.318597316",
+  "labels": [
+    {
+      "name": "bug",
+      "description": "bug",
+      "color": "#0075ca"
+    },
+    {
+      "name": "documentation",
+      "description": "documentation",
+      "color": "#008672"
+    }
+  ],
+  "closed": false
+}
+
+*/
