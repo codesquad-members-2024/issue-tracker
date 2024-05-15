17e70883539d0cf56307bb1e6c42caa6d8d3f22c
Keon Kim <fnc6488@gmail.com>
2024-05-15 17:14:41 +0900

    :recycle: refactor: 불필요한 어노테이션 제거
    @Value를 추가했기 때문에 불필요한 @Getter, @RequiredArgsConstructor 제거

--- BE/issue-tracker/src/main/java/codesquad/issuetracker/issue/IssueAttachedLabel.java
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/issue/IssueAttachedLabel.java
@@ -1,12 +1,8 @@
 package codesquad.issuetracker.issue;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Getter
-@RequiredArgsConstructor
 @Table("ISSUE_LABEL")
 @Value
 public class IssueAttachedLabel {
