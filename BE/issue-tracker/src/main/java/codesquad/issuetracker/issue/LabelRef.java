6b6456f92ab9a4aab6977fba09e6f86af546bc26
Keon Kim <fnc6488@gmail.com>
2024-05-15 16:42:30 +0900

    :sparkles: feat: LabelRef에 @Value 어노테이션 추가

--- BE/issue-tracker/src/main/java/codesquad/issuetracker/issue/LabelRef.java
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/issue/LabelRef.java
@@ -2,13 +2,15 @@ package codesquad.issuetracker.issue;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @RequiredArgsConstructor
 @Table("ISSUE_LABEL")
+@Value
 public class LabelRef {
 
-    private final Long labelId;
+    Long labelId;
 
 }
