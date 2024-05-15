19792689d0575ed95349bfa1006c36dcfa6dcc51
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-15 18:29:30 +0900

    feature Milestone 관련 커스텀 핸들러 추가

--- backend/src/main/java/com/CodeSquad/IssueTracker/Exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/Exception/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.CodeSquad.IssueTracker.Exception;
 
-import com.CodeSquad.IssueTracker.Exception.user.InvalidCredentialException;
-import com.CodeSquad.IssueTracker.Exception.user.InvalidUserFormatException;
-import com.CodeSquad.IssueTracker.Exception.user.UserAlreadyExistsException;
-import com.CodeSquad.IssueTracker.Exception.user.UserNotLoginException;
+import com.CodeSquad.IssueTracker.Exception.milestone.InvalidMilestoneRequestException;
+import com.CodeSquad.IssueTracker.Exception.milestone.MilestoneNotFoundException;
+import com.CodeSquad.IssueTracker.Exception.user.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,18 +16,29 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
-    }
-
-    @ExceptionHandler(InvalidCredentialException.class)
-    public ResponseEntity<String> handleAuthenticationFailedException(InvalidCredentialException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-    }
-
     @ExceptionHandler(UserNotLoginException.class)
     public ResponseEntity<String> handleUserNotLoginException(UserNotLoginException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidCredentialException.class)
+    public ResponseEntity<String> handleInvalidCredentialException(InvalidCredentialException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    //milestone
+    @ExceptionHandler(MilestoneNotFoundException.class)
+    public ResponseEntity<String> handleMilestoneNotFoundException(MilestoneNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidMilestoneRequestException.class)
+    public ResponseEntity<String> handleInvalidMilestoneRequestException(InvalidMilestoneRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
