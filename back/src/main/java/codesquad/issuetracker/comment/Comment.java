705e876c793937bd9b4751f265f8d34d0d8a8a12
yonghyeonpark <pyh4616@naver.com>
2024-05-15 15:37:26 +0900

    refactor: 작성 시간을 나타내는 필드 추가

--- back/src/main/java/codesquad/issuetracker/comment/Comment.java
+++ back/src/main/java/codesquad/issuetracker/comment/Comment.java
@@ -3,6 +3,8 @@ package codesquad.issuetracker.comment;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class Comment {
 
@@ -10,9 +12,11 @@ public class Comment {
     private Long id;
     private String writer;
     private String content;
+    private LocalDateTime createdDate;
 
     public Comment(String writer, String content) {
         this.writer = writer;
         this.content = content;
+        this.createdDate = LocalDateTime.now();
     }
 }
