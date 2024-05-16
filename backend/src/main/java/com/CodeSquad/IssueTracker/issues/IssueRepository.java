f4699cdb5935860212bc03a92fd3f745e6653b33
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-16 17:53:04 +0900

    feature issue가 만들어 질때 milestone DB에 있는 전체 issue 수를 1씩 증가하는 쿼리 추가

--- backend/src/main/java/com/CodeSquad/IssueTracker/issues/IssueRepository.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/issues/IssueRepository.java
@@ -1,5 +1,6 @@
 package com.CodeSquad.IssueTracker.issues;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -20,4 +21,8 @@ public interface IssueRepository extends CrudRepository<Issue, Long> {
 
     @Query("SELECT COUNT(issue_id) FROM issues WHERE is_closed = true")
     long countCloseIssues();
+
+    @Modifying
+    @Query("UPDATE milestone SET total_issue = total_issue + 1 WHERE milestone_id = :milestoneId")
+    void incrementIssueCountForMilestone(Long milestoneId);
 }
