882ab5f41dbf260cc74f3ee03ce8a2a7532f24a3
ehpark <silbroeh@gmail.com>
2024-05-17 02:33:22 +0900

    feat : 조회 기능 커스터마이징
    - soft delete 반영 조회 기능 추가

--- BE/src/main/java/com/codesquad/team3/issuetracker/support/repository/SoftDeleteCrudRepository.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/support/repository/SoftDeleteCrudRepository.java
@@ -1,6 +1,9 @@
 package com.codesquad.team3.issuetracker.support.repository;
 
 import com.codesquad.team3.issuetracker.global.entity.SoftDeleteEntity;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
@@ -12,5 +15,19 @@ public interface SoftDeleteCrudRepository<T extends SoftDeleteEntity, ID> extend
         update(entity);
 
         return entity;
+    }
+
+    default Iterable<T> getAll() {
+        return StreamSupport.stream(findAll().spliterator(), false)
+            .filter(entity -> !entity.isDeleted())
+            .collect(Collectors.toList());
+    }
+
+    default Optional<T> getById(ID id) {
+        Optional<T> optionalEntity = findById(id);
+        if (optionalEntity.isPresent() && optionalEntity.get().isDeleted()) {
+            return Optional.empty();
+        }
+        return optionalEntity;
     }
 }
