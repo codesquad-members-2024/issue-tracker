10cc8678a0bcf6b649a7824d7e2d7f3a3824e261
Keon Kim <fnc6488@gmail.com>
2024-05-16 13:01:02 +0900

    :recycle: refactor: IssueService 필드에 private final 키워드 추가

--- BE/issue-tracker/src/main/java/codesquad/issuetracker/issue/IssueService.java
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/issue/IssueService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class IssueService {
 
-    IssueRepository issueRepository;
+    private final IssueRepository issueRepository;
 
     @Autowired
     public IssueService(IssueRepository issueRepository) {
