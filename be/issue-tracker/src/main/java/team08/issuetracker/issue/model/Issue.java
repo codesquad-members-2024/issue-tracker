26901415d01d1e323fa557de4601151919751393
shimbaa <shim9597@gmail.com>
2024-05-16 16:11:11 +0900

    refactor: Issue 생성자 매개변수 변경, issue의 담당자 setter 구현
    1. Issue 생성자를 통해서는 클라이언트로 부터 받는 값 중 일대일 관계인 필드만 받는다
    2. 다대다 관계인 담당자 필드 (assignees) 는 setter를 통해서 설정한다

--- be/issue-tracker/src/main/java/team08/issuetracker/issue/model/Issue.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/issue/model/Issue.java
@@ -2,49 +2,44 @@ package team08.issuetracker.issue.model;
 
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.MappedCollection;
+import team08.issuetracker.issue.ref.Assignee;
 
 @Getter
 public class Issue {
     @Id
     private Long id;
-    private final boolean isOpen;// default : open 수정 할 때 만 클라이언트에서 값 받는다.
+    private final boolean isOpen; // default : open 수정 할 때 만 클라이언트에서 값 받는다.
     private final String title; //Non-Null
     private final String writer; //Non-Null
     private final String content;
     private final String uploadedFile;
-    private final Object milestoneId;
-//    private final List<Object> label;
-//    private final List<String> assignee;
     @Column(value = "TIMESTAMP")
     private final LocalDateTime createdTime;
 
-//    @MappedCollection(idColumn = "issue_id")
-//    private Set<IssueAttachedLabel> labels = new HashSet<>();
-//
-//    @MappedCollection(idColumn = "issue_id")
-//    private Set<Assignee> assignees = new HashSet<>();
+    @MappedCollection(idColumn = "issue_id")
+    private Set<Assignee> assignees;
 
-    public Issue(String title, String writer, String content, String uploadedFile, Object milestoneId, List<Object> label,
-                 List<String> assignee) {
+    public Issue(String title, String writer, String content, String uploadedFile) {
+
+        // issue 생성시 서버에서 초기화 값을 부여하는 필드들
         this.isOpen = true; // 이슈 생성시 true를 기본값으로 초기화 되어야 한다
-        this.createdTime = LocalDateTime.now(); // 이슈 생성시간을 저장한다
+        this.createdTime = LocalDateTime.now(); // 이슈 생성시, 생성 시간을 저장
 
-        // 클라이언트로 부터 받은 값들로 초기화 한다
-        // title 과 writer 를 제외한 값들은 nullable 이다
+        // issue 생성시 클라이언트에서 받은 값으로 초기화 하는 필드들
         this.title = title;
         this.writer = writer;
         this.content = content;
         this.uploadedFile = uploadedFile;
-        this.milestoneId = milestoneId;
-//        this.label = label;
-//        this.assignee = assignee;
+    }
+
+    // 다대다 관계는 setter를 통해 저장
+    public void setAssignees(Set<Assignee> assignees) {
+        this.assignees = assignees;
     }
 
     @Override
@@ -56,9 +51,7 @@ public class Issue {
                 ", writer='" + writer + '\'' +
                 ", content='" + content + '\'' +
                 ", uploadedFile='" + uploadedFile + '\'' +
-                ", milestone=" + milestoneId +
-//                ", label=" + label +
-//                ", assignee=" + assignee +
+                ", assignee=" + assignees +
                 ", createdTime=" + createdTime +
                 '}';
     }
