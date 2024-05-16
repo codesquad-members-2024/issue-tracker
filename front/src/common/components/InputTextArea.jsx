5d8db313bb856c33914f71a43b1272de991e3b70
melroh629 <jw.roh629@gmail.com>
2024-05-16 10:53:21 +0900

    fix: textarea 속성 수정

--- front/src/common/components/InputTextArea.jsx
+++ front/src/common/components/InputTextArea.jsx
@@ -1,18 +1,79 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Input } from 'antd';
 const { TextArea } = Input;
 
-export function InputTextArea({ className }) {
+export function InputTextArea({
+	className,
+	placeholder = '코멘트를 입력해 주세요✍🏻',
+	value,
+	onChange,
+	onClick,
+}) {
+	const [showCount, setShowCount] = useState(false);
+	const [wordsCount, setWordsCount] = useState(0);
+
+	const countWords = value => {
+		setWordsCount(value.length);
+	};
+	useEffect(() => {
+		countWords(value);
+	}, [value]);
 	return (
 		<StyledWrapper className={className}>
-			<StyledTextArea placeholder='코멘트를 입력하세요' showCount={true} />
+			<StyledTextArea
+				placeholder={placeholder}
+				value={value}
+				onChange={e => {
+					onChange(e);
+					countWords(e.target.value);
+				}}
+				onClick={onClick}
+				onBlur={() => {
+					setShowCount(false);
+				}}
+			/>
+			{showCount && <i>띄어쓰기 포함{wordsCount}자</i>}
 		</StyledWrapper>
 	);
 }
 const StyledWrapper = styled.div`
 	position: relative;
+
+	height: 132px;
+
+	// border: 1px solid ${({ $border }) => ($border ? '#003f66' : '#0075ca')};
+	.ant-input-outlined:focus-within {
+		box-shadow: none;
+	}
+	.ant-input-show-count {
+		${({ theme }) => theme.typography.medium[12]};
+		color: ${({ theme }) => theme.color.neutral.text.weak};
+	}
+	i {
+		position: absolute;
+		right: 44px;
+		bottom: 16px;
+		${({ theme }) => theme.typography.medium[12]};
+		color: ${({ theme }) => theme.color.neutral.text.weak};
+		font-style: normal;
+	}
 `;
 const StyledTextArea = styled(TextArea)`
-	min-height: 132px;
 	max-height: 500px;
+	height: 132px;
+	border: none;
+	&&& {
+		border-color: transparent;
+	}
+
+	&&&:hover {
+		border-color: transparent;
+	}
+
+	&&&:focus,
+	&&&:active {
+		border-color: transparent;
+		box-shadow: none;
+	}
 `;
