62e13492bc6370a8cb150966422bb4eec3ebd936
imojaek <imojaek@gmail.com>
2024-05-16 16:56:17 +0900

    feat: 열린 이슈 및 닫힌 이슈 개수 반환 추가
    get /filters

---
+++ backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
@@ -0,0 +1,23 @@
+package com.CodeSquad.IssueTracker.filter;
+
+import com.CodeSquad.IssueTracker.issues.IssueRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilterService {
+
+    IssueRepository issueRepository;
+
+    public FilterService(IssueRepository issueRepository) {
+        this.issueRepository = issueRepository;
+    }
+
+    public IssueNumberResponse getIssueNumber() {
+        long openIssueNum = issueRepository.countOpenIssues();
+        long closeIssueNum = issueRepository.countCloseIssues();
+        return IssueNumberResponse.builder()
+                .openedIssueNumber(openIssueNum)
+                .closedIssueNumber(closeIssueNum)
+                .build();
+    }
+}
