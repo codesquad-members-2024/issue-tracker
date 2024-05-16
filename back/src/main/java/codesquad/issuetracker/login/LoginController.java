a0a0495b04a1b701f87d69af6028e4fafac2bae2
choejieun <wldms5221@icloud.com>
2024-05-16 12:00:13 +0900

    refactor: import 수정

--- back/src/main/java/codesquad/issuetracker/login/LoginController.java
+++ back/src/main/java/codesquad/issuetracker/login/LoginController.java
@@ -1,11 +1,9 @@
 package codesquad.issuetracker.login;
 
-import codesquad.issuetracker.user.User;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
