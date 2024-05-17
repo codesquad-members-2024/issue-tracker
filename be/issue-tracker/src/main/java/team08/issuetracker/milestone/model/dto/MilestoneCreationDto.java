7d16ebd92d0bc9296497925ad902caa64a3822a8
Zoonmy <pjm2571@naver.com>
2024-05-17 16:52:24 +0900

    refactor: MilestoneCreationDtod에서 Milestone을 생성할 수 있도록 수정

--- be/issue-tracker/src/main/java/team08/issuetracker/milestone/model/dto/MilestoneCreationDto.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/milestone/model/dto/MilestoneCreationDto.java
@@ -1,6 +1,7 @@
 package team08.issuetracker.milestone.model.dto;
 
 import lombok.Getter;
+import team08.issuetracker.milestone.model.Milestone;
 
 import java.time.LocalDate;
 
@@ -8,4 +9,8 @@ public record MilestoneCreationDto(
         String name,
         String description,
         LocalDate completeDate
-) {}
+) {
+    public Milestone toEntity() {
+        return new Milestone(name, description, completeDate);
+    }
+}
