23a8c631731eaa291294d7ee1b40232ab88bd11d
choejieun <wldms5221@icloud.com>
2024-05-17 14:47:03 +0900

    refactor: LoginRepository 삭제

--- back/src/main/java/codesquad/issuetracker/login/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package codesquad.issuetracker.login;
-
-import codesquad.issuetracker.user.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoginRepository extends CrudRepository<User, String> {
-    User findByLoginId(String loginId);
-}
