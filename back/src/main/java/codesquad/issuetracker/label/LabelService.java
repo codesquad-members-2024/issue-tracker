0905f93894842432890a807e82a86820d4dc8991
Daniel <923daniel@naver.com>
2024-05-17 10:17:27 +0900

    feat: Label CRUD 구현
    - LabelController에서 Dto 생성

--- back/src/main/java/codesquad/issuetracker/label/LabelService.java
+++ back/src/main/java/codesquad/issuetracker/label/LabelService.java
@@ -3,6 +3,8 @@ package codesquad.issuetracker.label;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class LabelService {
@@ -10,6 +12,22 @@ public class LabelService {
     private final LabelRepository labelRepository;
 
     public void createLabel(Label label) {
-        labelRepository.insert(label.getName(), label.getDescription(), label.getColor());
+        labelRepository.save(label);
+    }
+
+    public List<Label> getAllLabels() {
+        return (List<Label>) labelRepository.findAll();
+    }
+
+    public Label getLabelById(Long labelId) {
+        return labelRepository.findById(labelId).orElseThrow(RuntimeException::new);
+    }
+
+    public void updateLabelById(Label updatedLabel) {
+        labelRepository.save(updatedLabel);
+    }
+
+    public void deleteLabelById(Long labelId) {
+        labelRepository.deleteById(labelId);
     }
 }
