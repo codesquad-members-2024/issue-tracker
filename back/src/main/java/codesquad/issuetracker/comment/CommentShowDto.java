28787ead28e0b7c1ccf988fda6823be4cb8bcddc
yonghyeonpark <pyh4616@naver.com>
2024-05-17 13:57:47 +0900

    refactor: CommentShowDto에 profileImage 필드 추가 및 임시 데이터 저장

--- back/src/main/java/codesquad/issuetracker/comment/CommentShowDto.java
+++ back/src/main/java/codesquad/issuetracker/comment/CommentShowDto.java
@@ -10,11 +10,13 @@ public class CommentShowDto {
     private String writer;
     private String content;
     private String duration;
+    private String profileImage;
 
     public CommentShowDto(Comment comment) {
         this.id = comment.getId();
         this.writer = comment.getLoginId();
         this.content = comment.getContent();
         this.duration = TimeCalculator.calculateTimeDifference(comment.getCreatedDate());
+        this.profileImage = "https://avatars.githubusercontent.com/u/126778700?s=40&v=4"; // 테스트용 데이터
     }
 }
