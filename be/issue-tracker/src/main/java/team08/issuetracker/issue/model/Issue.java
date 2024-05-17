f9cd17646e96676cb73198cc01db4cdc8ae6b383
shimbaa <shim9597@gmail.com>
2024-05-17 14:12:54 +0900

    fix: 자동 작성되는 sql문 오류로 인해 column 이름값 변경

--- be/issue-tracker/src/main/java/team08/issuetracker/issue/model/Issue.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/issue/model/Issue.java
@@ -4,24 +4,26 @@ package team08.issuetracker.issue.model;
 import java.time.LocalDateTime;
 import java.util.Set;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.MappedCollection;
 import team08.issuetracker.issue.ref.Assignee;
 
+@Setter
 @Getter
 public class Issue {
     @Id
     private Long id;
-    private final boolean isOpen; // default : open 수정 할 때 만 클라이언트에서 값 받는다.
-    private final String title; //Non-Null
+    private boolean isOpen; // default : open 수정 할 때 만 클라이언트에서 값 받는다.
     private final String writer; //Non-Null
+    private final String title; //Non-Null
     private final String content;
     private final String uploadedFile;
     @Column(value = "TIMESTAMP")
-    private final LocalDateTime createdTime;
+    private LocalDateTime createdTime;
 
-    @MappedCollection(idColumn = "issue_id")
+    @MappedCollection(idColumn = "ISSUE_ID")
     private Set<Assignee> assignees;
 
     public Issue(String title, String writer, String content, String uploadedFile) {
