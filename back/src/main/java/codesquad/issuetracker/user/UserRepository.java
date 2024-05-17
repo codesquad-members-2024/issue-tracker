69b74af16a22b3c202bfad834b1421313dc31e34
choejieun <wldms5221@icloud.com>
2024-05-17 14:58:53 +0900

    refactor: UserRepository에 findByLoginId 추가

--- back/src/main/java/codesquad/issuetracker/user/UserRepository.java
+++ back/src/main/java/codesquad/issuetracker/user/UserRepository.java
@@ -3,5 +3,6 @@ package codesquad.issuetracker.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, String> {
+    User findByLoginId(String loginId);
 
 }
