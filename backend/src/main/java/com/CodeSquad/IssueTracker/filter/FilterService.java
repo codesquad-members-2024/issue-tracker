d04fa2d03103f1afa769101f21d77a4235987bc8
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-17 13:31:32 +0900

    feature Filter 를 FilterResponse 객체를 내보내기 위한 메서드를 추가 하였습니다.

--- backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
@@ -1,15 +1,22 @@
 package com.CodeSquad.IssueTracker.filter;
 
 import com.CodeSquad.IssueTracker.issues.IssueRepository;
+import com.CodeSquad.IssueTracker.milestone.Milestone;
+import com.CodeSquad.IssueTracker.milestone.MilestoneRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FilterService {
 
     IssueRepository issueRepository;
+    MilestoneRepository milestoneRepository;
 
-    public FilterService(IssueRepository issueRepository) {
+    public FilterService(IssueRepository issueRepository, MilestoneRepository milestoneRepository) {
         this.issueRepository = issueRepository;
+        this.milestoneRepository = milestoneRepository;
     }
 
     public IssueNumberResponse getIssueNumber() {
@@ -18,6 +25,27 @@ public class FilterService {
         return IssueNumberResponse.builder()
                 .openIssueCount(openIssueNum)
                 .closeIssueCount(closeIssueNum)
+                .build();
+    }
+
+    public List<MilestoneFilterResponse> getMilestoneFilter() {
+        List<Milestone> openMilestone = milestoneRepository.findAllOpenMilestones();
+        List<MilestoneFilterResponse> milestoneFilterResponses = new ArrayList<>();
+
+        for (Milestone milestone : openMilestone) {
+            milestoneFilterResponses.add (MilestoneFilterResponse.builder()
+                    .milestoneId(milestone.getMilestoneId())
+                    .title(milestone.getTitle())
+                    .build());
+        }
+
+        return milestoneFilterResponses;
+    }
+
+    public FilterResponse getFilterResponse() {
+        return FilterResponse.builder()
+                .issueNumberResponse(getIssueNumber())
+                .milestoneFilterResponse(getMilestoneFilter())
                 .build();
     }
 }
