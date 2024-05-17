98ad757e257c2733c12e392456fc597e34cef6ae
shimbaa <shim9597@gmail.com>
2024-05-17 17:17:20 +0900

    feat: 모든 이슈 조회, 임시 응답 포맷 구현

--- be/issue-tracker/src/main/java/team08/issuetracker/issue/controller/IssueController.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/issue/controller/IssueController.java
@@ -1,11 +1,14 @@
 package team08.issuetracker.issue.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import team08.issuetracker.issue.model.Issue;
 import team08.issuetracker.issue.model.dto.IssueCreationDto;
 import team08.issuetracker.issue.service.IssueService;
 
@@ -14,6 +17,17 @@ import team08.issuetracker.issue.service.IssueService;
 @RequiredArgsConstructor
 public class IssueController {
     private final IssueService issueService;
+
+    @GetMapping("/issue")
+    public ResponseEntity<String> issues() {
+        List<Issue> issues = issueService.issues();
+        StringBuilder sb = new StringBuilder();
+        for (Issue issue : issues) {
+            sb.append(issue);
+            sb.append("\n");
+        }
+        return ResponseEntity.ok(sb.toString());
+    }
 
     @PostMapping("/issue")
     public ResponseEntity<String> createIssue(@RequestBody IssueCreationDto issueCreationDto) {
