3241845733d991e3bbbddccea5cf5b695361c567
yonghyeonpark <pyh4616@naver.com>
2024-05-19 17:59:13 +0900

    feat: 업데이트 쿼리문을 이용한 Isse title 수정 메서드 구현"

--- back/src/main/java/codesquad/issuetracker/issue/IssueRepository.java
+++ back/src/main/java/codesquad/issuetracker/issue/IssueRepository.java
@@ -1,6 +1,12 @@
 package codesquad.issuetracker.issue;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IssueRepository extends CrudRepository<Issue, Long> {
+
+    @Modifying
+    @Query("update issue set title = :newTitle where id = :issueId")
+    void updateTitleById(Long issueId, String newTitle);
 }
