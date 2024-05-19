b33b0ad2315a3715e563638f4594a9ed15fa2ede
Daniel <923daniel@naver.com>
2024-05-19 09:35:27 +0900

    feat: milestone 엔티티 클래스 구현

--- back/src/main/java/codesquad/issuetracker/milestone/Milestone.java
+++ back/src/main/java/codesquad/issuetracker/milestone/Milestone.java
@@ -1,21 +1,37 @@
 package codesquad.issuetracker.milestone;
 
+import codesquad.issuetracker.issue.Issue;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 public class Milestone {
 
     @Id
+    private Long id;
     private String name;
-    private String dueDate;
-    private boolean isValid;
+    private String description;
+    private LocalDateTime createTime;
+    private LocalDateTime dueDate;
+    private boolean isClosed;
+    @MappedCollection(idColumn = "milestone_id")
+    private Set<Issue> issues;
 
-    public Milestone(String name,
-                     String dueDate,
-                     boolean isValid) {
+    public Milestone(Long id,
+                     String name,
+                     String description,
+                     LocalDateTime createTime,
+                     LocalDateTime dueDate,
+                     boolean isClosed) {
+        this.id = id;
         this.name = name;
+        this.description = description;
+        this.createTime = createTime;
         this.dueDate = dueDate;
-        this.isValid = isValid;
+        this.isClosed = isClosed;
     }
 }
