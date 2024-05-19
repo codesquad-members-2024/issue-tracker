73ee1862dba1e21866047c4584331301615a057a
Daniel <923daniel@naver.com>
2024-05-19 09:46:35 +0900

    feat: MilestoneRepository 인터페이스 구현

---
+++ back/src/main/java/codesquad/issuetracker/milestone/MilestoneRepository.java
@@ -0,0 +1,6 @@
+package codesquad.issuetracker.milestone;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MilestoneRepository extends CrudRepository<Milestone, Integer> {
+}
