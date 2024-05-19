93b302060ef58c1c09b90a60d2d2be021dc5d299
yonghyeonpark <pyh4616@naver.com>
2024-05-19 20:08:09 +0900

    feat: feat: 업데이트 쿼리문을 이용한 Issue content 수정 메서드 구현

--- back/src/main/java/codesquad/issuetracker/issue/IssueRepository.java
+++ back/src/main/java/codesquad/issuetracker/issue/IssueRepository.java
@@ -9,4 +9,8 @@ public interface IssueRepository extends CrudRepository<Issue, Long> {
     @Modifying
     @Query("update issue set title = :newTitle where id = :issueId")
     void updateTitleById(Long issueId, String newTitle);
+
+    @Modifying
+    @Query("update issue set content = :newContent where id = :issueId")
+    void updateContentById(Long issueId, String newContent);
 }
