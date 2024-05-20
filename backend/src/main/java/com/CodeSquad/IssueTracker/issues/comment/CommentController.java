30f90ac728e031153c068368fea8bf0a4d13247b
imojaek <imojaek@gmail.com>
2024-05-20 19:44:49 +0900

    fix: 코멘트 수정 관련 메소드 삭제

--- backend/src/main/java/com/CodeSquad/IssueTracker/issues/comment/CommentController.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/issues/comment/CommentController.java
@@ -20,12 +20,4 @@ public class CommentController {
         commentService.addComment(request, issueId);
         return ResponseEntity.ok().build();
     }
-
-    @PutMapping("/issue/{issueId}/comment/{commentId}")
-    public ResponseEntity<Void> updateCommnet(@RequestBody CommentCreateRequest request,
-                                              @PathVariable("issueId") Long issueId,
-                                              @PathVariable("commentId") Long commentId) {
-        commentService.updateComment(request, issueId, commentId);
-        return ResponseEntity.ok().build();
-    }
 }
