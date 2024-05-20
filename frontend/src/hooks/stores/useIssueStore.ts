7a51cc0e114231120c62473f42c8555b0f6e9347
jueun-park98 <jooeun06161@gmail.com>
2024-05-20 11:50:53 +0900

    feat: 마일스톤 응답 처리 추가

--- frontend/src/hooks/stores/useIssueStore.ts
+++ frontend/src/hooks/stores/useIssueStore.ts
@@ -13,21 +13,30 @@ interface IssueCounts {
   closeIssueCount: number;
 }
 
+interface Milestone {
+  milestoneId: number;
+  title: string;
+}
+
 interface IssueStoreState {
   issues: Headline[];
   openIssueCount: number;
   closeIssueCount: number;
+  milestones: Milestone[];
   setIssues: (issues: Headline[]) => void;
   setIssueCounts: ({ openIssueCount, closeIssueCount }: IssueCounts) => void;
+  setMilestones: (milestones: Milestone[]) => void;
 }
 
 const useIssueStore = create<IssueStoreState>((set) => ({
   issues: [],
   openIssueCount: 0,
   closeIssueCount: 0,
+  milestones: [],
   setIssues: (issues: Headline[]) => set(() => ({ issues })),
   setIssueCounts: ({ openIssueCount, closeIssueCount }: IssueCounts) =>
     set(() => ({ openIssueCount, closeIssueCount })),
+  setMilestones: (milestones: Milestone[]) => set(() => ({milestones})),
 }));
 
 export default useIssueStore;
