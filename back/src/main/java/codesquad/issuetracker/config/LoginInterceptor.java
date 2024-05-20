a4f5cd4c01eb59c4477a3928143cef9a37426345
choejieun <wldms5221@icloud.com>
2024-05-20 13:43:02 +0900

    refactor: OPTIONS 수정 및 ok 추가

--- back/src/main/java/codesquad/issuetracker/config/LoginInterceptor.java
+++ back/src/main/java/codesquad/issuetracker/config/LoginInterceptor.java
@@ -9,11 +9,16 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("loginId") == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
+        response.setStatus(HttpServletResponse.SC_OK);
         return true;
     }
 }
