9006dee46e0a124579a18bf1181960d186de69c7
growth-mango <esudatcom@gmail.com>
2024-05-20 18:10:55 +0900

    refactor: Dto 클래스의 클래스명 접미사 통일, LabelResponse -> LabelOverviewDto 로 변경

--- be/issue-tracker/src/main/java/team08/issuetracker/label/model/dto/LabelOverviewDto.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/label/model/dto/LabelOverviewDto.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-public class LabelResponse {
+public class LabelOverviewDto {
     private LabelCountDto labelCount;
     private List<LabelDto> labels;
 
-    public LabelResponse(LabelCountDto labelCount, List<LabelDto> labels) {
+    public LabelOverviewDto(LabelCountDto labelCount, List<LabelDto> labels) {
         this.labelCount = labelCount;
         this.labels = labels;
     }
