4a1a2a384c7bb2835f7ca610e9ece3eac4305f78
choejieun <wldms5221@icloud.com>
2024-05-20 12:20:01 +0900

    refactor: OPTIONS 추가

--- back/src/main/java/codesquad/issuetracker/config/WebConfig.java
+++ back/src/main/java/codesquad/issuetracker/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 와일드카드
                 .allowedOrigins("http://3.38.30.88:8080", "http://*:3000", "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
