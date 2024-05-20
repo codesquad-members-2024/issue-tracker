3e64a8423299eb3f7bf40aac1603bbfc85f693cc
kalia <fmoon11200@naver.com>
2024-05-20 17:52:43 +0900

    feat JDBC 레포지토리 생성 findById, removeLabelFromIssue, addLAbelToIssue

---
+++ backend/src/main/java/com/CodeSquad/IssueTracker/issues/issueLabel/IssueLabelRepository.java
@@ -0,0 +1,28 @@
+package com.CodeSquad.IssueTracker.issues.issueLabel;
+
+import com.CodeSquad.IssueTracker.issues.issueLabel.dto.LabelRequest;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface IssueLabelRepository extends CrudRepository<IssueLabel, Long> {
+
+    @Query("select * from issueLabel where issue_id = :issueId")
+    List<LabelRequest> findByIssueId(Long issueId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM issueLabel WHERE issue_id = :issueId AND label_id = :labelId")
+    void removeLabelFromIssue(@Param("issueId") Long issueId, @Param("labelId") Long labelId);
+
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO issueLabel (issue_id, label_id) VALUES (:issueId, :labelId)")
+    int addLabelToIssue(@Param("issueId") Long issueId, @Param("labelId") Long labelId);
+}
