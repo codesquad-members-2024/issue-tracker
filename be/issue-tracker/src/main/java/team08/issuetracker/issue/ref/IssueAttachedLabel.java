d75ebcd1994da2725ab6383dd955d8f986ad1df2
shimbaa <shim9597@gmail.com>
2024-05-20 14:52:07 +0900

    refactor: IssueAttachedLabel 값 설정 방식 변경
    setter 제거, 생성자를 통한 값 설정

--- be/issue-tracker/src/main/java/team08/issuetracker/issue/ref/IssueAttachedLabel.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/issue/ref/IssueAttachedLabel.java
@@ -1,14 +1,14 @@
 package team08.issuetracker.issue.ref;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Getter
-@Setter
+@RequiredArgsConstructor
 public class IssueAttachedLabel {
     @Id
     private Long id;
-    private Long issueId;
-    private Long labelId;
+    private final Long issueId;
+    private final Long labelId;
 }
