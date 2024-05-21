bc3540158c3552711362768765516ce367399449
hiidy <hiidy115@gmail.com>
2024-05-21 13:43:56 +0900

    :sparkles: feat: 라벨 목록 불러오기에 대한 테스트코드 추가
    - 라벨 목록을 못찾을 때 예외처리 테스트코드 추가
    - 라벨 목록을 불러오는 테스트 코드 추가

--- BE/issue-tracker/src/main/java/codesquad/issuetracker/exception/ResourceNotFoundException.java
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package codesquad.issuetracker.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException() {
         super("요청하신 데이터를 찾을 수 없습니다.");
