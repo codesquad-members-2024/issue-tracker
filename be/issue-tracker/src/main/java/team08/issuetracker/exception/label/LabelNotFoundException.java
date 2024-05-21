f3f64c6035dd9a6195cc3b6ca71b5af4797006fc
growth-mango <esudatcom@gmail.com>
2024-05-21 17:34:08 +0900

    refactor: LabelNotFoundException 과 에러 메세지 레벨 통일시키도록 수정

--- be/issue-tracker/src/main/java/team08/issuetracker/exception/label/LabelNotFoundException.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/exception/label/LabelNotFoundException.java
@@ -1,7 +1,7 @@
 package team08.issuetracker.exception.label;
 
 public class LabelNotFoundException extends RuntimeException {
-    private static final String ERROR_MESSAGE = "해당 id의 라벨을 찾을 수 없습니다.";
+    private static final String ERROR_MESSAGE = "라벨이 존재하지 않습니다.";
 
     public LabelNotFoundException() {
         super(ERROR_MESSAGE);
