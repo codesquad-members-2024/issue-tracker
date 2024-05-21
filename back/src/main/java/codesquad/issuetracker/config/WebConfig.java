69a122a820691160dbd51636f5bc62367694b744
choejieun <wldms5221@icloud.com>
2024-05-21 15:26:04 +0900

    refactor: WebConfig ip 수정

--- back/src/main/java/codesquad/issuetracker/config/WebConfig.java
+++ back/src/main/java/codesquad/issuetracker/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 와일드카드
-                .allowedOrigins("http://3.38.30.88:8080", "http://*:3000", "http://localhost:5173")
+                .allowedOrigins("http://3.38.30.88:8080", "http://192.168.45.216:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
