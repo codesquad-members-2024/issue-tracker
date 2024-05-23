a3cdd934663d4ef68b780a1bd7d627ba18f29792
horang <fnelclsrn123@gmail,com>
2024-05-23 20:04:40 +0900

    refactor : member dto 생성
    -프론트에 전해줄 데이터 담을 member dto 생성

---
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/member/dto/response/MemberDetail.java
@@ -0,0 +1,12 @@
+package com.codesquad.team3.issuetracker.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MemberDetail {
+
+    private Integer id;
+    private String memberId;
+}
