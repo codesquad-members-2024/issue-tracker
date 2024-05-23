d9a9f49a876b788594b175cbdde0b8344e3f148a
imojaek <imojaek@gmail.com>
2024-05-23 18:26:34 +0900

    feat: 로그인 인터셉터에서 OPTIONS 메소드를 예외처리

--- backend/src/main/java/com/CodeSquad/IssueTracker/interceptor/CheckLoginInterceptor.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/interceptor/CheckLoginInterceptor.java
@@ -9,6 +9,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class CheckLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            return true; // OPTIONS 요청은 바로 통과시킴
+        }
+
         HttpSession session = request.getSession();
         if (session.getAttribute("userId") == null) {
             throw new UserNotLoginException("로그인이 필요합니다.");
