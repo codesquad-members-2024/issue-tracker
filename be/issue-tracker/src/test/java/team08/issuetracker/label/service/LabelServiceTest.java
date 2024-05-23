49b9c401177cd7617b8ddae51ea99892c70b3bd1
growth-mango <esudatcom@gmail.com>
2024-05-23 19:14:59 +0900

    refactor: test에 DisplayName 추가

--- be/issue-tracker/src/test/java/team08/issuetracker/label/service/LabelServiceTest.java
+++ be/issue-tracker/src/test/java/team08/issuetracker/label/service/LabelServiceTest.java
@@ -1,6 +1,7 @@
 package team08.issuetracker.label.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +26,7 @@ class LabelServiceTest {
     private LabelRepository labelRepository;
 
     @Test
+    @DisplayName("라벨이 성공적으로 생성되어야 한다.")
     void testCreateLabel() {
         LabelCreationRequest labelCreationRequest = new LabelCreationRequest("New Label", "New Description", "#FFFFFF", true);
         Label expectedLabel = new Label("New Label", "New Description", "#FFFFFF", true);
@@ -37,6 +39,7 @@ class LabelServiceTest {
     }
 
     @Test
+    @DisplayName("주어진 아이디로 라벨을 찾을 수 없을 때, LabelNotFoundException을 던져야 한다.")
     void testLabelNotFound() {
         when(labelRepository.findById(1L)).thenReturn(Optional.empty());
 
@@ -46,6 +49,7 @@ class LabelServiceTest {
     }
 
     @Test
+    @DisplayName("주어진 아이디에 해당하는 라벨을 성공적으로 삭제해야 한다.")
     void testDeleteLabel() {
         Long id = 1L;
         Label labelToDelete = new Label("Name", "Description", "#FFFFFF", true);
