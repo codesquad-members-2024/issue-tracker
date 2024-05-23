b3529e4439b6ec57ee60dd7f2e1e4e28e30a5926
growth-mango <esudatcom@gmail.com>
2024-05-23 19:29:52 +0900

    feat: CommentRepository 구현

---
+++ be/issue-tracker/src/main/java/team08/issuetracker/comment/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package team08.issuetracker.comment.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import team08.issuetracker.comment.model.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+    List<Comment> findByIssueId(Long issueId);
+    Optional<Comment> findByIdAndIssueId(Long id, Long issueId);
+    void deleteByIdAndIssueId(Long id, Long issueId);
+}
