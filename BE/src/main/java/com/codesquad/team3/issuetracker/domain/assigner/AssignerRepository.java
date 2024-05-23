4276a00cae39c8d3650e1ab60f46aa5f22489cbb
horang <fnelclsrn123@gmail,com>
2024-05-23 20:05:49 +0900

    refactor : assiner를 위한 클래스 생성
    -issue 담당자를 담는 테이블을 위한 클래스 생성

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/assigner/AssignerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codesquad.team3.issuetracker.domain.assigner;
-
-import com.codesquad.team3.issuetracker.support.repository.SimpleCrudRepository;
-
-public interface AssignerRepository extends SimpleCrudRepository<Assigner, Integer> {
-}
