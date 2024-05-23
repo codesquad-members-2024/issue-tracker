d2435db1798011899ea0423d40f1878758512d72
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-24 00:37:47 +0900

    feature 커스텀 filter 관련 메서드 추가

--- backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
@@ -1,29 +1,27 @@
 package com.CodeSquad.IssueTracker.filter;
 
 import com.CodeSquad.IssueTracker.filter.dto.FilterListResponse;
-import com.CodeSquad.IssueTracker.issues.IssueRepository;
+import com.CodeSquad.IssueTracker.issues.Issue;
 import com.CodeSquad.IssueTracker.issues.IssueService;
 import com.CodeSquad.IssueTracker.labels.LabelService;
-import com.CodeSquad.IssueTracker.milestone.Milestone;
-import com.CodeSquad.IssueTracker.milestone.MilestoneRepository;
 import com.CodeSquad.IssueTracker.milestone.MilestoneService;
-import com.CodeSquad.IssueTracker.user.UserService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FilterService {
-
+    private final CustomFilterRepository customFilterRepository;
     IssueService issueService;
     LabelService labelService;
     MilestoneService milestoneService;
 
-    public FilterService(IssueService issueService, LabelService labelService, MilestoneService milestoneService) {
+    public FilterService(IssueService issueService, LabelService labelService,
+                         MilestoneService milestoneService, CustomFilterRepository customFilterRepository) {
         this.issueService = issueService;
         this.labelService = labelService;
         this.milestoneService = milestoneService;
+        this.customFilterRepository = customFilterRepository;
     }
 
     public FilterListResponse getFilterListResponse() {
@@ -33,5 +31,11 @@ public class FilterService {
                 .milestoneListResponse(milestoneService.getOpenMilestoneList())
                 .authorListResponse(issueService.getAuthorList())
                 .build();
+    }
+
+    public List<Issue> findFilteredIssues(Boolean isClosed, String assignee,
+                                          List<String> labelTitles, String milestoneTitle,
+                                          String author){
+        return customFilterRepository.findFilteredIssues(isClosed, assignee, labelTitles, milestoneTitle, author);
     }
 }
