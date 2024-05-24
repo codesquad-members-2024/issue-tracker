d8c67f0bd18d4510984c23e54d6f865b0d372f57
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-24 11:29:20 +0900

    refactor conflict로 발생한 에러 해결

--- backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/filter/FilterService.java
@@ -1,26 +1,30 @@
 package com.CodeSquad.IssueTracker.filter;
 
-import com.CodeSquad.IssueTracker.assignee.AssigneeService;
 import com.CodeSquad.IssueTracker.filter.dto.FilterListResponse;
 import com.CodeSquad.IssueTracker.issues.Issue;
 import com.CodeSquad.IssueTracker.issues.IssueService;
 import com.CodeSquad.IssueTracker.labels.LabelService;
 import com.CodeSquad.IssueTracker.milestone.MilestoneService;
+import com.CodeSquad.IssueTracker.user.UserService;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class FilterService {
     private final CustomFilterRepository customFilterRepository;
     IssueService issueService;
     LabelService labelService;
     MilestoneService milestoneService;
+    UserService userService;
 
     public FilterService(IssueService issueService, LabelService labelService,
-                         MilestoneService milestoneService, CustomFilterRepository customFilterRepository) {
+                         MilestoneService milestoneService, CustomFilterRepository customFilterRepository,
+                         UserService userService) {
         this.issueService = issueService;
         this.labelService = labelService;
         this.milestoneService = milestoneService;
         this.customFilterRepository = customFilterRepository;
+        this.userService = userService;
     }
 
     public FilterListResponse getFilterListResponse() {
