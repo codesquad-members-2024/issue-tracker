083e4e5e653197589748034f82c5a98d7ad8277a
growth-mango <esudatcom@gmail.com>
2024-05-24 11:22:36 +0900

    feat: Comment 업데이트 서비스 로직 구현 (주어진 이슈아이디, 아이디에 해당하는 코멘트 찾고 -> 찾은 코멘트 업데이트 데이터로 갱신하고(entity) -> 업데이트 데이터 저장하고)

--- be/issue-tracker/src/main/java/team08/issuetracker/comment/service/CommentService.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/comment/service/CommentService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team08.issuetracker.comment.model.Comment;
 import team08.issuetracker.comment.model.dto.CommentCreationRequest;
+import team08.issuetracker.comment.model.dto.CommentUpdateRequest;
 import team08.issuetracker.comment.repository.CommentRepository;
+import team08.issuetracker.exception.comment.CommentNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,18 @@ public class CommentService {
         Comment comment = commentCreationRequest.toEntity();
 
         // 2) 저장 및 반환
+        return commentRepository.save(comment);
+    }
+
+    public Comment updateComment(Long issueId, Long id, CommentUpdateRequest commentUpdateRequest) {
+        // 1) 주어진 issueId와 id에 해당하는 코멘트 찾기
+        Comment comment = commentRepository.findByIdAndIssueId(id, issueId)
+                .orElseThrow(CommentNotFoundException::new);
+
+        // 2) 찾은 코멘트 업데이트 데이터로 갱신하기
+        comment.update(commentUpdateRequest);
+
+        // 3) 업데이트 내용 저장 및 반환
         return commentRepository.save(comment);
     }
 }
