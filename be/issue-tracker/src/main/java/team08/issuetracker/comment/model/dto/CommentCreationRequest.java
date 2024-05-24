96b0f66443bc4aaca288bbe95b1ae852e02e7d2b
growth-mango <esudatcom@gmail.com>
2024-05-24 11:59:44 +0900

    fix: Comment 생성 시, createdAt null 로 들어가던 문제 해결 -> entity에서 자동 생성 되는데 dto 에서 entity로 변환할 때도 createdAt 값을 넘겨주어서 값을 설정하지 않을 경우 null로 들어가던 문제 해결

--- be/issue-tracker/src/main/java/team08/issuetracker/comment/model/dto/CommentCreationRequest.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/comment/model/dto/CommentCreationRequest.java
@@ -6,16 +6,17 @@ import team08.issuetracker.comment.model.Comment;
 
 import java.time.LocalDateTime;
 
+/* 📌 writer 랑, issueId 는 각 각 로그인한 사용자 정보, 이슈 정보를 받아오는건데... 어떻게 해야 할지 감이 오지 않음
+* 우선 writer 는 그냥 입력 받는 걸로 틀 잡고, issueId 는 pathvariable id 를 setId 하는 걸로 구현*/
 @Data
 @AllArgsConstructor
 public class CommentCreationRequest {
-    private String writer;
-    private Long issueId;
+    private String writer; // fk
+    private Long issueId; // fk
     private String content;
-    private LocalDateTime createdAt;
     private String uploadedFile;
 
     public Comment toEntity() {
-        return new Comment(writer, issueId, content, createdAt, uploadedFile);
+        return new Comment(writer, issueId, content, uploadedFile);
     }
 }
