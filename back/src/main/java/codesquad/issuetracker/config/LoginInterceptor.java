e429efe528194a302fb178b7003497374e77a075
Daniel <923daniel@naver.com>
2024-05-25 19:10:03 +0900

    feat: JWT 토큰을 이용한 LoginInterceptor 구현

--- back/src/main/java/codesquad/issuetracker/config/LoginInterceptor.java
+++ back/src/main/java/codesquad/issuetracker/config/LoginInterceptor.java
@@ -1,24 +1,31 @@
 package codesquad.issuetracker.config;
 
+import codesquad.issuetracker.login.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@RequiredArgsConstructor
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
+    private final JwtUtil jwtUtil;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("Login Check Interceptor {}", request.getRequestURI());
-        HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("LOGIN USER") == null) {
-            log.info("미인증 사용자 요청");
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        String authorization = request.getHeader("Authorization");
+        String accessToken = jwtUtil.getJwtTokenFromHeader(authorization);
+
+        try {
+            String token = jwtUtil.validateToken(accessToken); // JWT 토큰 검증
+            request.setAttribute("loginId", token); // loginId 값 저장
+            return true;
+        } catch (Exception e) {
+            log.info("인증되지 않은 토큰");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 유효하지 않은 JWT 토큰이면 401에러 반환
             return false;
         }
-        response.setStatus(HttpServletResponse.SC_OK);
-        return true;
     }
 }
