af8c61a3c082e20c496a18b869b84f3da671898a
hiidy <hiidy115@gmail.com>
2024-05-26 17:38:10 +0900

    :bug:  fix: 마일스톤 dueDate의 잘못된 타입을수정
    - LocalDateTime에서 LocalDate로 타입 수정

--- BE/issue-tracker/src/main/java/codesquad/issuetracker/milestone/dto/MilestoneRequest.java
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/milestone/dto/MilestoneRequest.java
@@ -2,6 +2,7 @@ package codesquad.issuetracker.milestone.dto;
 
 import codesquad.issuetracker.base.State;
 import codesquad.issuetracker.milestone.Milestone;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.Value;
 
@@ -10,13 +11,13 @@ public class MilestoneRequest {
 
     String title;
     String description;
-    LocalDateTime dueDate;
+    LocalDate dueDate;
 
     public Milestone toEntity() {
         return Milestone.builder()
             .title(title)
             .description(description)
-            .dueDate(dueDate)
+            .dueDate(dueDate.atStartOfDay())
             .updatedAt(LocalDateTime.now())
             .state(State.OPEN)
             .build();
