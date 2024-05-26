609dd17cb3b6b159e0a9efed4500a6949a02dc94
shimbaa <shim9597@gmail.com>
2024-05-26 23:15:14 +0900

    refactor: 이슈 제목 변경시 요청 유효하지 않은 값에 대한 예외 이름 변경

--- be/issue-tracker/src/main/java/team08/issuetracker/exception/issue/InvalidIssueTitleUpdateFormException.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/exception/issue/InvalidIssueTitleUpdateFormException.java
@@ -1,9 +1,9 @@
 package team08.issuetracker.exception.issue;
 
-public class InvalidIssueUpdateFormException extends RuntimeException{
+public class InvalidIssueTitleUpdateFormException extends RuntimeException{
     private static final String ERROR_MESSAGE = "이슈의 제목은 필수값 입니다.";
 
-    public InvalidIssueUpdateFormException() {
+    public InvalidIssueTitleUpdateFormException() {
         super(ERROR_MESSAGE);
     }
 }
--- be/issue-tracker/src/main/java/team08/issuetracker/issue/model/dto/update/IssueTitleUpdateRequest.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/issue/model/dto/update/IssueTitleUpdateRequest.java
@@ -1,12 +1,12 @@
 package team08.issuetracker.issue.model.dto.update;
 
-import team08.issuetracker.exception.issue.InvalidIssueUpdateFormException;
+import team08.issuetracker.exception.issue.InvalidIssueTitleUpdateFormException;
 
 public record IssueTitleUpdateRequest(String title) {
 
     public IssueTitleUpdateRequest {
         if (title == null || title.isEmpty()) {
-            throw new InvalidIssueUpdateFormException();
+            throw new InvalidIssueTitleUpdateFormException();
         }
     }
 }
