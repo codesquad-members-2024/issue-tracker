208aa7ec427441e538cda20494c0bea7e565b532
shimbaa <shim9597@gmail.com>
2024-05-26 23:25:20 +0900

    refactor: 담당자 변경 요청 값을 받는 자료구조 수정
    기존 List -> 변경 Set

--- be/issue-tracker/src/main/java/team08/issuetracker/issue/model/dto/update/IssueAssigneeUpdateRequest.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/issue/model/dto/update/IssueAssigneeUpdateRequest.java
@@ -1,11 +1,10 @@
 package team08.issuetracker.issue.model.dto.update;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import team08.issuetracker.issue.ref.Assignee;
 
-public record IssueAssigneeUpdateRequest(List<String> assigneeIds) {
+public record IssueAssigneeUpdateRequest(Set<String> assigneeIds) {
 
     public Set<Assignee> getAssigneesOfIssue(long issueId) {
         return this.assigneeIds.stream()
