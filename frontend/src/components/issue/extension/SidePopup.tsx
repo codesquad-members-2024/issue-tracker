9c27f172d254294942a06e448031d83d2cba051b
jueun-park98 <jooeun06161@gmail.com>
2024-05-27 19:15:00 +0900

    feat: 새로운 이슈 작성 및 이슈 상세 페이지 사이드바 출력 기능 추가

--- frontend/src/components/issue/extension/SidePopup.tsx
+++ frontend/src/components/issue/extension/SidePopup.tsx
@@ -1,5 +1,89 @@
-function SidePopup() {
-  return (<div></div>)
+import React from "react";
+import styled from "styled-components";
+
+type PopupType = "assignee" | "label" | "milestone";
+
+interface SidePopupProps {
+  popupType: PopupType;
+  items?: string[] | object[];
 }
+
+const HEADER_NAME = {
+  assignee: "담당자",
+  label: "레이블",
+  milestone: "마일스톤",
+};
+
+const TITLE_KEY = {
+  label: "labelName",
+  milestone: "title",
+};
+
+const SidePopup = React.forwardRef<HTMLDivElement, SidePopupProps>((props, ref) => {
+  const { popupType, items } = props;
+
+  return (
+    <Wrapper ref={ref}>
+      <div>
+        <Header>{HEADER_NAME[popupType]} 설정</Header>
+        <ScrollableArea>
+          {items?.map((item) => (
+            <SelectOption>
+              {typeof item === "string"
+                ? item
+                : popupType === "assignee"
+                  ? JSON.stringify(item)
+                  : item[TITLE_KEY[popupType] as keyof typeof item]}
+            </SelectOption>
+          ))}
+        </ScrollableArea>
+      </div>
+    </Wrapper>
+  );
+});
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 5.125em;
+  left: 1em;
+  width: 15em;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #d9dbe9;
+  border-radius: 1em;
+  overflow: hidden;
+  z-index: 100;
+`;
+
+const ScrollableArea = styled.div`
+  max-height: 30em;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Header = styled.div`
+  width: calc(100% - 2.666em);
+  height: 2.666em;
+  padding: 0 1.333em;
+  font-size: 0.75em;
+  display: flex;
+  align-items: center;
+  background-color: #eff0f6;
+  color: #6e7191;
+  border-bottom: 1px solid #d9dbe9;
+`;
+
+const SelectOption = styled.div`
+  height: 2.75em;
+  padding: 0 1em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fefefe;
+  color: #4e4b66;
+  border-top: 1px solid #d9dbe9;
+`;
 
 export default SidePopup;
--- frontend/src/components/issue/extension/Sidebar.tsx
+++ frontend/src/components/issue/extension/Sidebar.tsx
@@ -1,20 +1,68 @@
 import styled from "styled-components";
 import plusIcon from "../../../img/icon/plusIcon_dark.svg";
+import { useEffect, useRef, useState } from "react";
+import SidePopup from "./SidePopup";
+import useIssueStore from "../../../hooks/stores/useIssueStore";
+
+type SidebarKeys = "assignee" | "label" | "milestone";
 
 function Sidebar() {
+  const { users, labels, milestones } = useIssueStore();
+
+  const [filterbarVisible, setFilterbarVisible] = useState<Record<SidebarKeys, boolean>>({
+    assignee: false,
+    label: false,
+    milestone: false,
+  });
+
+  const sidebarRefs = {
+    assignee: useRef<HTMLDivElement>(null),
+    label: useRef<HTMLDivElement>(null),
+    milestone: useRef<HTMLDivElement>(null),
+  };
+
+  const handleRightMenuClick = (menu: SidebarKeys) => {
+    setFilterbarVisible((prev) => ({
+      ...prev,
+      [menu]: true,
+    }));
+  };
+
+  const handleClickOutside = ({ target }: Event) => {
+    Object.keys(sidebarRefs).forEach((key) => {
+      const ref = sidebarRefs[key as SidebarKeys];
+      if (ref.current && !ref.current.contains(target as Node)) {
+        setFilterbarVisible((prev) => ({
+          ...prev,
+          [key]: !prev[key as SidebarKeys],
+        }));
+      }
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <Wrapper>
       <Sector>
         <span>담당자</span>
-        <img src={plusIcon} />
+        <img src={plusIcon} onClick={() => handleRightMenuClick("assignee")} />
+        {filterbarVisible.assignee && <SidePopup ref={sidebarRefs.assignee} popupType="assignee" items={users} />}
       </Sector>
       <Sector>
         <span>레이블</span>
-        <img src={plusIcon} />
+        <img src={plusIcon} onClick={() => handleRightMenuClick("label")} />
+        {filterbarVisible.label && <SidePopup ref={sidebarRefs.label} popupType="label" items={labels} />}
       </Sector>
       <Sector>
         <span>마일스톤</span>
-        <img src={plusIcon} />
+        <img src={plusIcon} onClick={() => handleRightMenuClick("milestone")} />
+        {filterbarVisible.milestone && <SidePopup ref={sidebarRefs.milestone} popupType="milestone" items={milestones} />}
       </Sector>
     </Wrapper>
   );
@@ -26,10 +74,10 @@ const Wrapper = styled.div`
   height: 16em;
   border: 1px solid #d9dbe9;
   border-radius: 16px;
-  overflow: hidden;
 `;
 
 const Sector = styled.div`
+  position: relative;
   padding: 2em;
   border-top: 1px solid #d9dbe9;
   display: flex;
