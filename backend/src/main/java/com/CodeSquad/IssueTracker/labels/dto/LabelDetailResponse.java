5fe5ca58661afda138ca2972dc2465b2f96f2609
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-27 15:03:46 +0900

    refactor 응답을 보낼때 DTO를 통해 보내도록 수정

---
+++ backend/src/main/java/com/CodeSquad/IssueTracker/labels/dto/LabelDetailResponse.java
@@ -0,0 +1,9 @@
+package com.CodeSquad.IssueTracker.labels.dto;
+
+import lombok.Builder;
+
+@Builder
+public record LabelDetailResponse(Long labelId, String labelName,
+                                  String description, String textColor,
+                                  String bgColor)
+{ }
