ef5de99208dfe886723bca0d4c352617107078f3
Gyeongtaek12345 <ogt124321@gmail.com>
2024-05-28 14:33:14 +0900

    refactor 주석 제거및 생성자 추가

--- backend/src/main/java/com/CodeSquad/IssueTracker/issues/dto/IssueDetailAccess.java
+++ backend/src/main/java/com/CodeSquad/IssueTracker/issues/dto/IssueDetailAccess.java
@@ -1,13 +1,11 @@
 package com.CodeSquad.IssueTracker.issues.dto;
 
-import com.CodeSquad.IssueTracker.issues.issueLabel.dto.LabelRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-
-//i.issue_id, i.title, i.author, i.published_at, i.is_closed, i.milestone_id, " +
-//            "a.user_id AS assignee, l.label_id , l.label_name, l.bg_color, l.text_color
+@AllArgsConstructor
 @Getter
 public class IssueDetailAccess {
     private Long issueId;
