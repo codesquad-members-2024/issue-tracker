acff171de6c7b61149477cb296a78b24177f52c3
growth-mango <esudatcom@gmail.com>
2024-05-28 16:41:56 +0900

    refactor: CommentSummaryDto 가 Comment 객체 직적 받아 초기화 하도록 리팩토링

--- be/issue-tracker/src/main/java/team08/issuetracker/comment/model/dto/CommentSummaryDto.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/comment/model/dto/CommentSummaryDto.java
@@ -2,14 +2,21 @@ package team08.issuetracker.comment.model.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import team08.issuetracker.comment.model.Comment;
 
 import java.time.LocalDateTime;
 
 @Getter
-@RequiredArgsConstructor
 public class CommentSummaryDto {
     private final String writer;
     private final String content;
     private final LocalDateTime createdAt;
     private final String imageUrl;
+
+    public CommentSummaryDto(Comment comment, String imageUrl) {
+        this.writer = comment.getWriter();
+        this.content = comment.getContent();
+        this.createdAt = comment.getCreatedAt();
+        this.imageUrl = imageUrl;
+    }
 }
