2f75e36ad8c7af30bbd3e9d8460d4729630520aa
Zoonmy <pjm2571@naver.com>
2024-05-29 23:10:39 +0900

    refactor: Comment Controller Import 와일드 문 수정, comment 주석에 대한 기능 구현이 완료되어 주석 제거 - Comment 주석에 대한 기능 구현 완료되어, 주석 제거 - import *; 삭제, 각각이 import 되도록 리팩토링

--- be/issue-tracker/src/main/java/team08/issuetracker/comment/controller/CommentController.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/comment/controller/CommentController.java
@@ -3,9 +3,17 @@ package team08.issuetracker.comment.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import team08.issuetracker.comment.model.Comment;
-import team08.issuetracker.comment.model.dto.*;
+import team08.issuetracker.comment.model.dto.CommentCreationRequest;
+import team08.issuetracker.comment.model.dto.CommentCreationResponse;
+import team08.issuetracker.comment.model.dto.CommentUpdateRequest;
+import team08.issuetracker.comment.model.dto.CommentUpdateResponse;
 import team08.issuetracker.comment.service.CommentService;
 
 @RestController // ResponseBody + Controller
@@ -38,12 +46,5 @@ public class CommentController {
 
         return ResponseEntity.ok(response);
     }
-
-    /*
-     * 📌 조회 : 개별 이슈 조회 시 코멘트 조회 . . . => 리스트로 이슈에 넘겨주기
-     * 📌 삭제 : 개별 이슈 삭제 시 종속된 코멘트도 함께 삭제 . . . => 이슈에서 구현
-     * 📌 개별 코멘트 삭제 기능은 기획안에 존재하지 않음
-     * */
-
 
 }
