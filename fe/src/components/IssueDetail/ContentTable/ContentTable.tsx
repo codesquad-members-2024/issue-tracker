dccf38671947806e15199fbcde179ddd3f656f48
mi-hye <satge13@gmail.com>
2024-05-29 17:50:52 +0900

    refactor: 코멘트 부분 참조 데이터 수정

--- fe/src/components/IssueDetail/ContentTable/ContentTable.tsx
+++ fe/src/components/IssueDetail/ContentTable/ContentTable.tsx
@@ -1,35 +1,31 @@
+import getTimeStamp from "../../../utility/getTimeStamp";
 import Button from "../../common/Button";
 import InformationTag from "../../common/InformationTag";
 
 const border = "component-border dark:component-border--dark";
 
 interface PropsType {
-	writer: string;
-	timeSince: string;
-	content: string;
-	comment: boolean;
+	isWriter: boolean;
+	issue?: Issue;
+	comment?: IssueComment;
 }
 
-function ContentTable({ writer, timeSince, content, comment }: PropsType) {
+function ContentTable({ isWriter, issue, comment }: PropsType) {
+	const data: Issue | IssueComment = (issue ? issue : comment)!;
 	return (
 		<div className={`${border} border-[1px] rounded-2xl`}>
 			<div className={`h-[64px] ${border} border-b-[1px] flex items-center justify-between`}>
 				<div className="flex ml-5 font-medium">
-					{/*TODO : db의 유저프로필로 변경예정 */}
-					<img
-						className="w-[32px] h-[32px] rounded-full"
-						alt="userProfile"
-						src="https://s3-alpha-sig.figma.com/img/bfa1/72b0/77fbdbfc84f8ad555402b23fb6c7a0ed?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eI0HusP8AQJhfrYkbdft4etLT-322gDp7B7Px-jCgKq9YxT-2fFKD4o6AhzmnVaFjLWGiHP0xS~kATP~GzdJOyVdsfc4UEryn1QuF2T9PmoEdt0ZnUR7bqsSHuOReoVWy67p4Drl~meTCSGbWn8amC1-vFCT23Coy9HLU9fkNA0r3uh47-NMSV-Wx7IwUF202FHxOo027XQFyYGP9Xu56j19~mvu0d9TAlW~oHGscTheXQL5afzDdwBFrEGbMgU2Lli2QKdpkrDnjUKb0mRtqWOAVPU45~RZnFemwVP2UKq~e9Q68Q5u4zzvqrlcXbcTyHjkgYGiD6vSTPX-AlMiHA__"
-					/>
+					<img className="w-[32px] h-[32px] rounded-full" alt="userProfile" src={data.imageUrl} />
 					<span className="mx-3 flex items-center text-grayscale.700 dark:text-grayscale.400">
-						{writer}
+						{data.writer}
 					</span>
 					<span className="flex items-center text-grayscale.600 dark:text-grayscale.500">
-						{timeSince}
+						{getTimeStamp(data.createdAt)}
 					</span>
 				</div>
 				<div className="mr-5 flex items-center justify-between">
-					{comment && (
+					{isWriter && (
 						<div className="mr-3">
 							<InformationTag text="작성자" icon={null} fillColor="#FEFEFE" textBright={false} />
 						</div>
@@ -41,7 +37,7 @@ function ContentTable({ writer, timeSince, content, comment }: PropsType) {
 				</div>
 			</div>
 			<pre className="font-[inherit] bg-grayscale.50 dark:bg-grayscale.800 text-grayscale.700 dark:text-grayscale.400 rounded-b-2xl p-5">
-				{content}
+				{data.content}
 			</pre>
 		</div>
 	);
--- fe/src/routes/IssueDetail.tsx
+++ fe/src/routes/IssueDetail.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header/Header";
+import IssueDetailContent from "../components/IssueDetail/IssueDetailContent";
 import IssueDetailTitle from "../components/IssueDetail/IssueDetailTitle";
 import Loading from "../components/common/Loading";
 import useGet from "../hooks/useGet";
@@ -9,13 +10,13 @@ function IssueDetail() {
 	const { data, error, isLoading } = useGet(`issue/${issueId}`, `/issue/${issueId}`, true);
 	if (isLoading) return <Loading />;
 	if (error) return <div>에러 {error.message}</div>;
-	console.log(data);
+	console.log(data); // DELETE
 	return (
 		<div className="w-screen h-screen flex items-center justify-center overflow-auto">
 			<div className="h-[95%] w-[85%]">
 				<Header />
-				<IssueDetailTitle issue={data.issue} commentLegth={data.comments.length}/>
-				{/* <IssueDetailContent issue={issue} timeSince={timeSince} /> */}
+				<IssueDetailTitle issue={data.issue} commentLegth={data.comments.length} />
+				<IssueDetailContent issueData={data} />
 			</div>
 		</div>
 	);
