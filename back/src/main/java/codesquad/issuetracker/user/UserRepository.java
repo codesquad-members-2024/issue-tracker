82389a18f6fc9fef64b5570c01c74fcf07fd6ec2
Daniel <923daniel@naver.com>
2024-05-30 14:52:42 +0900

    feat: User가 존재하는지 확인하는 쿼리 구현

--- back/src/main/java/codesquad/issuetracker/user/UserRepository.java
+++ back/src/main/java/codesquad/issuetracker/user/UserRepository.java
@@ -1,13 +1,22 @@
 package codesquad.issuetracker.user;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, String> {
-    User findByLoginId(String loginId);
 
     List<User> findAll();
 
     List<User> findAllById(Iterable<String> ids);
+
+    User findByLoginId(String loginId);
+
+    @Modifying
+    @Query("INSERT INTO user(login_id, password, profile_image) VALUES (:loginId, :password, :profileImage)")
+    void save(String loginId, String password, String profileImage);
+
+    boolean existsByLoginId(String loginId);
 }
