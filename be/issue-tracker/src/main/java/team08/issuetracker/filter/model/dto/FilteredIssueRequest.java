de6a03837638adc177c4f9c4ec048012020be10a
Zoonmy <pjm2571@naver.com>
2024-05-30 16:26:15 +0900

    feat: filter에서 라벨, 마일스톤, 담당자가 없는 필터 기능 구현 - assignee : 빈 문자열 -> 없는 상태 - label_id : 0 -> 라벨 없는 상태 - milestone_id : 0 -> 마일스톤 없는 상태

--- be/issue-tracker/src/main/java/team08/issuetracker/filter/model/dto/FilteredIssueRequest.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/filter/model/dto/FilteredIssueRequest.java
@@ -24,30 +24,49 @@ public class FilteredIssueRequest {
             query.append(" WHERE is_open = ").append(state.equalsIgnoreCase("opened"));
             hasCondition = true;
         }
+
         if (writer != null) {
             query.append(hasCondition ? " AND " : " WHERE ");
             query.append("writer = '").append(writer).append("'");
             hasCondition = true;
         }
+
         if (assignee != null) {
             query.append(hasCondition ? " AND " : " WHERE ");
-            query.append("id IN (SELECT issue_id FROM assignee WHERE member_id = '").append(assignee).append("')");
+            if (assignee.isEmpty()) {
+                query.append("id NOT IN (SELECT issue_id FROM assignee)");
+            } else {
+                query.append("id IN (SELECT issue_id FROM assignee WHERE member_id = '").append(assignee).append("')");
+            }
             hasCondition = true;
         }
+
         if (commenter != null) {
             query.append(hasCondition ? " AND " : " WHERE ");
             query.append("id IN (SELECT issue_id FROM comment WHERE writer = '").append(commenter).append("')");
             hasCondition = true;
         }
+
         if (labelId != null) {
             query.append(hasCondition ? " AND " : " WHERE ");
-            query.append("id IN (SELECT issue_id FROM issue_attached_label WHERE label_id = '").append(labelId).append("')");
+            if (labelId == 0) {
+                query.append("id NOT IN (SELECT issue_id FROM issue_attached_label)");
+            } else {
+                query.append("id IN (SELECT issue_id FROM issue_attached_label WHERE label_id = '").append(labelId).append("')");
+            }
             hasCondition = true;  // 플래그 업데이트 추가
         }
+
         if (milestoneId != null) {
             query.append(hasCondition ? " AND " : " WHERE ");
-            query.append("milestone_id = ").append(milestoneId);
+            if (milestoneId == 0) {
+                query.append("milestone_id IS NULL");
+            } else {
+                query.append("milestone_id = ").append(milestoneId);
+            }
         }
+
+        System.out.println(query);
 
         return query.toString();
     }
