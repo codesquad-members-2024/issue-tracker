bdef78cc0a3daae513008ba78140fecb4a5b19ef
Daniel <923daniel@naver.com>
2024-05-31 12:53:52 +0900

    refactor: WebConfig에 불필요한 origins 제거

--- back/src/main/java/codesquad/issuetracker/config/WebConfig.java
+++ back/src/main/java/codesquad/issuetracker/config/WebConfig.java
@@ -10,17 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    
+
     private final JwtUtil jwtUtil;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 와일드카드
                 .allowedOrigins(
-                        "http://3.38.30.88:8080",
                         "http://localhost:3000",
                         "http://localhost:5173",
-                        "http://192.168.1.25:5173",
                         "http://fe-issue-tracker-s3.s3-website.ap-northeast-2.amazonaws.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
